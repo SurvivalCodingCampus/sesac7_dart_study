c8119d3765dc4cc2449f8556bfded34668b893f6
Hagmin.Lee <iilhm2001@naver.com>
2025-06-04 20:43:56 +0900

    chore: cleric.dart 수정(하드코딩부 제거, 변동 없는 값은 상수로 변경)

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -25,16 +25,16 @@ class Cleric {
     int result = 0; // 실질 회복량. 0초 기도를 해도 값이 나올 수 있도록 0으로 초기화.
 
     if(second > 0) { // 기도 시간이 양수일 때만 실질적인 마법 사용.
-      int mpGen = second + Random().nextInt(3); // mp 회복 전체양(초과분도 포함), 기도 시간 + 0~2 랜덤 정수
+      final int mpGen = second + Random().nextInt(3); // mp 회복 전체양(초과분도 포함), 기도 시간 + 0~2 랜덤 정수
 
-      if(mp+mpGen <= 10) { // 10까지는 mpGen 값이 그대로 회복량이 됨.
+      if(mp + mpGen <= 10) { // 10까지는 mpGen 값이 그대로 회복량이 됨.
         mp += mpGen;
         result = mpGen;
       }
       else { // 회복했을 때 10을 넘어갈 경우
-        int temp = mp; // 회복 전 mp 값을 임시로 저장하고,
-        mp = 10; // mp를 최대치까지 회복한 후,
-        result = maxMp-temp; // mp 최대치(10) - 회복 전 mp 값(temp)을 반환.
+        final int temp = mp; // 회복 전 mp 값을 임시로 저장하고,
+        mp = maxMp; // mp를 최대치까지 회복한 후,
+        result = maxMp - temp; // mp 최대치(10) - 회복 전 mp 값(temp)을 반환.
       }
     }
 
