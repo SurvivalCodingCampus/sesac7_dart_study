cf4aa75e9ac2376ced3cefa6107d4b5b88dbe86e
wtoshm <wtoshm@gmail.com>
2025-06-04 19:56:12 +0900

    Refactoring pray method

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -18,22 +18,44 @@ class Cleric {
     }
   }
 
-  int pray(int prayTime) {
-    int mpPrayPoint = 0;
-    if (prayTime > 0) {
-      mpPrayPoint = prayTime + Random().nextInt(3);
+  // int pray(int prayTime) {
+  //   int mpPrayPoint = 0;
+  //   if (prayTime > 0) {
+  //     mpPrayPoint = prayTime + Random().nextInt(3);
+  //   } else {
+  //     print(" pray 시간은 1초 이상이어야 합니다.");
+  //   }
+  //
+  //   int maxMpCanBeRecovered = maxMp - mp;
+  //
+  //   if(mpPrayPoint > maxMpCanBeRecovered) {
+  //     mp = mp + maxMpCanBeRecovered;
+  //     return maxMpCanBeRecovered;
+  //   } else {
+  //     mp = mp + mpPrayPoint;
+  //     return mpPrayPoint;
+  //   }
+  // }
+
+  int pray(int praySeconds) {
+
+    // 리턴될 mpPointsRecovered 정의 및 계산
+    int mpPointsRecovered = 0;
+    if (praySeconds > 0) {
+      mpPointsRecovered = praySeconds + Random().nextInt(3);
     } else {
-      print(" pray 시간은 1초 이상이어야 합니다.");
+      print("pray 시간은 1초이상이어야 합니다.");
     }
 
-    int maxMpCanBeRecovered = maxMp - mp;
+    // 최대 mpPointsRecovered 정의 및 계산
+    int maxMpPointsRecovered = maxMp - mp;
 
-    if(mpPrayPoint > maxMpCanBeRecovered) {
-      mp = mp + maxMpCanBeRecovered;
-      return maxMpCanBeRecovered;
-    } else {
-      mp = mp + mpPrayPoint;
-      return mpPrayPoint;
+    // mpPointsRecovered 가 최대 maxMpPointsRecovered 보다 클 경우
+    // mpPointsRecovered 를 최대 maxMpPointsRecovered 로 재할당
+    if (mpPointsRecovered > maxMpPointsRecovered) {
+      mpPointsRecovered = maxMpPointsRecovered;
     }
+
+    return mpPointsRecovered;
   }
 }
