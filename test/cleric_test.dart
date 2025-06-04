ddc7d7f39c499f994711a090cf6e3697eb751757
Giteak <rlxor159@gmail.com>
2025-06-04 19:22:08 +0900

    test: Cleric Class TestCase 추가

--- test/cleric_test.dart
+++ test/cleric_test.dart
@@ -4,17 +4,66 @@ import 'package:modu_3_dart_study/cleric.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Cleric Test', () {
-    // given(준비)
-    final cleric = Cleric("클레릭");
+  group('Cleric Test', () {
+    test('최초 Cleric 인스턴스 생성 후 속성값 확인', () {
+      var clericName = 'Cleric';
+      final cleric = Cleric(clericName);
 
-    // when(실행)
-    cleric.selfAid();
-    print('after use selfAid mp:: ${cleric.mp}');
-    cleric.pray(Random().nextInt(15));
-    print('after use pray mp:: ${cleric.mp}');
+      expect(cleric.name, equals(clericName));
+      expect(cleric.mp, equals(Cleric.maxMp)); // 최초 mp 값이 MaxMp값과 동일한지 확인
+      expect(cleric.hp, equals(Cleric.maxHp)); // 최초 hp 값이 MaxHp값과 동일한지 확인
+    });
 
-    // then(검증)
-    expect(cleric.mp <= 10, equals(true));
+    test('selfAid 호출 시 최소 mp 값 검증', () {
+      final cleric = Cleric('Cleric');
+
+      cleric.mp = 3;
+      final int currentMp = cleric.mp;
+
+      cleric.selfAid();
+
+      expect(cleric.mp, equals(currentMp));
+    });
+
+    test('selfAid 호출 시 hp값 maxHp값 반영 검증', () {
+      final cleric = Cleric('Cleric');
+
+      cleric.hp = 15;
+
+      cleric.selfAid();
+
+      expect(cleric.hp == Cleric.maxHp, equals(true));
+    });
+
+    test('pray 호출 시 mp 회복 값 반영 검증', () {
+      final cleric = Cleric('Cleric');
+
+      cleric.mp -= 6;
+      final int beforeClericMp = cleric.mp;
+
+      final int actualMp = cleric.pray(2); // 회복된 mp 값
+
+      expect(cleric.mp == beforeClericMp + actualMp, equals(true));
+    });
+
+    test('pray 호출 후 현재 mp가 회복 요청시간 + 0 ~ 2 만큼 추가 되었는 지 검증', () {
+      final cleric = Cleric('Cleric');
+
+      cleric.mp = 4;
+
+      int actualMp = cleric.pray(3);
+
+      expect(actualMp, lessThanOrEqualTo(5));
+    });
+
+    test('pray 호출 후 현재 회복된 mp가 maxMp 값을 넘긴는 지 검증', () {
+      final cleric = Cleric('Cleric');
+
+      cleric.mp = Random().nextInt(Cleric.maxMp); // Cleric mp값을 maxMp값 내에서 랜덤으로 지정
+
+      cleric.pray(Random().nextInt(100));
+
+      expect(cleric.mp, lessThanOrEqualTo(Cleric.maxMp));
+    });
   });
 }
