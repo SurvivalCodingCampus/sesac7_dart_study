4c7d8f81415505345c3395cc1baea5e7910b4303
Jaesung lim <7newer@gmail.com>
2025-06-04 21:55:35 +0900

    feat: cleric.dart pray메서드 작성

--- test/250604/cleric_test.dart
+++ test/250604/cleric_test.dart
@@ -4,20 +4,20 @@ import 'package:test/test.dart';
 void main(){
   test('Cleric class Test',(){
     //given(준비)
-    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+    final cleric = Cleric('성직자',1,2); // 인스턴스 생성
 
     //when(실행)
 
     //then(검증)
     // 생성자, 초기화 확인
-    expect(cleric.name, '용사');
+    expect(cleric.name, '성직자');
     expect(cleric.hp, 50);
     expect(cleric.mp, 10);
   });
 
   test('selfAid() 메서드 Test',(){
     //given(준비)
-    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+    final cleric = Cleric('성직자',1,2); // 인스턴스 생성
 
     //when(실행)
     cleric.selfAid();
@@ -30,7 +30,8 @@ void main(){
 
   test('selfAid() 메서드 2번 반복 Test',(){
     //given(준비)
-    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+    final cleric = Cleric('성직자',1,2); // 인스턴스 생성
+    cleric.hp = 10;
 
     //when(실행)
     for (int i = 0; i < 2; i++){
@@ -45,7 +46,8 @@ void main(){
 
   test('selfAid() 메서드 3번 반복 Test',(){
     //given(준비)
-    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+    final cleric = Cleric('성직자',1,2); // 인스턴스 생성
+    cleric.hp = 10;
 
     //when(실행)
     for (int i = 0; i < 3; i++){
@@ -58,4 +60,29 @@ void main(){
     expect(cleric.mp, 0);
   });
 
+  test('pray 메서드, mp가 10보다 작은 경우 Test',(){
+    //given(준비)
+    var cleric = Cleric('성직자', 1, 1);
+
+    //when(실행)
+    cleric.mp = 0;
+    cleric.Pray(10);
+
+    //then(검증)
+    //expect(cleric.mp, isNot(10));
+    expect(cleric.mp < 10, true);
+
+  });
+
+  test('pray 메서드, mp가 10보다 큰 경우 Test',(){
+    //given(준비)
+    var cleric = Cleric('성직자', 1, 1);
+
+    //when(실행)
+    cleric.mp = 10;
+    cleric.Pray(1);
+
+    //then(검증)
+    expect(cleric.mp, 10);
+  });
 }
