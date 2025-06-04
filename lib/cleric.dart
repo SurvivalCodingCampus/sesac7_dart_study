853f65e37ad8820448c2653e1d208c5f936f1c0d
SibangGyeon <briankim0695@gmail.com>
2025-06-04 19:17:02 +0900

    refactor: Cleric 클래스 생성자 수정, pray() 조건문 로직 수정

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -7,35 +7,40 @@ class Cleric {
   int mp;
   final int maxMp = 10;
 
-  Cleric(this.name, this.hp, this.mp);
+  Cleric(this.name, this.hp, this.mp) {
+    if (hp <= 0) {
+      hp = 1;
+    }
+    if (hp > maxHp) {
+      hp = maxHp;
+    }
+    if (mp < 0) {
+      mp = 0;
+    }
+    if (mp > maxMp) {
+      mp = maxMp;
+    }
+  }
 
   void selfAid() {
-    if (this.mp >= 5) {
-      this.mp -= 5;
-      this.hp = this.maxHp;
+    if (mp >= 5) {
+      mp -= 5;
+      hp = maxHp;
     }
   }
 
   int pray(int time) {
-    // 기도 시간은 최대 초당 1씩 회복 된다는 기준
-    // 기도는 mp가 최대가 아닐 경우만 가능
-    if (((time > 0) && (time <= this.maxMp)) && this.mp < this.maxMp) {
-      int previousMp = this.mp;
-      int recoveryMp;
-
-      // 회복 보정 0 ~ 2
-      this.mp += time + Random().nextInt(3);
-
-      // maxMp 초과 회복 불가
-      if (this.mp > this.maxMp) {
-        this.mp = this.maxMp;
-      }
-
-      recoveryMp = this.mp - previousMp;
-
-      return recoveryMp;
-    } else {
+    // 기도 시간 유효성 검사 및 MP가 최대가 아닌 경우만 허용
+    if (time <= 0 || mp >= maxMp) {
       return 0;
     }
+    int previousMp = mp;
+    // 회복 보정 0 ~ 2
+    mp += time + Random().nextInt(3);
+    // maxMp 초과 회복 불가
+    if (mp > maxMp) {
+      mp = maxMp;
+    }
+    return mp - previousMp;
   }
 }
