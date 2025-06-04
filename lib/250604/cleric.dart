6e6c29f933d2e5194d3f3075200ff7051ef54584
Jaesung lim <7newer@gmail.com>
2025-06-04 17:53:06 +0900

    feat: cleric.dart selfAid()작성 및 테스트코드작성

--- lib/250604/cleric.dart
+++ lib/250604/cleric.dart
@@ -5,6 +5,8 @@
 3. Cleric 클래스에 “이름", “HP”, “최대 HP”, “MP”, “최대 MP”를 속성으로 추가 하시요.
 4. 또한 HP와 최대 HP는 정수로 초기치 50, MP와 최대 MP는 정수로 초기치 10으로 하고,
 5. 최대 HP와 최대 MP는 상수 필드로 선언 하시오.
+6. 성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.
+7. 연습 1 에 선언한 Cleric 클래스에 “selfAid()” 메소드를 추가 하시오. 또한, 이 메소드는 인수가 없고, 리턴 값도 없다.
 */
 class Cleric{
 
@@ -15,9 +17,21 @@ class Cleric{
   final int maxHp = 50; // 최대 HP, 초기치(50), 상수 선언
   final int maxMp = 10; // 최대 MP, 초기치(10), 상수 선언
 
-  //생성자
+  // 생성자
   Cleric(this.name, this.hp, this.mp){
     hp = maxHp;
     mp = maxMp;
+  }
+
+  // selfAid메서드
+  // 호출시 mp에서 -5를 더하고 HP를 최대치로 설정
+  // mp가 0또는 이하가 될 경우 소모 문구표시
+  void selfAid(){
+    if(mp>=0){
+      mp -= 5;
+      hp = maxHp;
+    }else{
+      print("mp가 전부 소모 되었습니다.");
+    }
   }
 }
--- test/250604/cleric_test.dart
+++ test/250604/cleric_test.dart
@@ -2,7 +2,7 @@ import 'package:modu_3_dart_study/250604/cleric.dart';
 import 'package:test/test.dart';
 
 void main(){
-  test('Cleric Test',(){
+  test('Cleric class Test',(){
     //given(준비)
     final cleric = Cleric('용사',1,2); // 인스턴스 생성
 
@@ -14,4 +14,48 @@ void main(){
     expect(cleric.hp, 50);
     expect(cleric.mp, 10);
   });
+
+  test('selfAid() 메서드 Test',(){
+    //given(준비)
+    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+
+    //when(실행)
+    cleric.selfAid();
+
+    //then(검증)
+    //selfAid() 결과 확인
+    expect(cleric.hp, 50);
+    expect(cleric.mp, 5);
+  });
+
+  test('selfAid() 메서드 2번 반복 Test',(){
+    //given(준비)
+    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+
+    //when(실행)
+    for (int i = 0; i < 2; i++){
+      cleric.selfAid();
+    }
+
+    //then(검증)
+    //selfAid() 결과 확인
+    expect(cleric.hp, 50);
+    expect(cleric.mp, 0);
+  });
+
+  test('selfAid() 메서드 3번 반복 Test',(){
+    //given(준비)
+    final cleric = Cleric('용사',1,2); // 인스턴스 생성
+
+    //when(실행)
+    for (int i = 0; i < 3; i++){
+      cleric.selfAid();
+    }
+
+    //then(검증)
+    //selfAid() 결과 확인
+    expect(cleric.hp, 50);
+    expect(cleric.mp, 0);
+  });
+
 }
