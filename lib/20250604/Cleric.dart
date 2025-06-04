47eb1249b12d25b5371cd7ac07696cd629804419
sehwan <sehwanq7@gmail.com>
2025-06-04 15:10:47 +0900

    feat: selfAid 기능 추가

--- lib/20250604/Cleric.dart
+++ lib/20250604/Cleric.dart
@@ -6,4 +6,13 @@ class Cleric {
   final maxMp = 10;
 
   Cleric(this.name, this.hp, this.mp);
+
+  void selfAid() {
+    if(mp >= 5) {
+      mp = mp - 5;
+      hp = maxHp;
+    } else {
+      print('mp가 부족합니다.');
+    }
+  }
 }
