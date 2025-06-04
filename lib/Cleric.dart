16b392b65026a83132388f02ef94818584191ea3
Giteak <rlxor159@gmail.com>
2025-06-04 15:34:04 +0900

    feat: selfAid 메서드 추가

--- lib/Cleric.dart
+++ lib/Cleric.dart
@@ -6,4 +6,13 @@ class Cleric {
   static const int maxMp = 10;
 
   Cleric(this.name);
+
+  void selfAid() {
+    if (mp >= 5) {
+      mp -= 5;
+      hp = maxHp;
+    } else {
+      print('mp 소모 불가능');
+    }
+  }
 }
