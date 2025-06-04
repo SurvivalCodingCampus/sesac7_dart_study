c3adf5cc938d9dae63344b509229ec88dcd057b8
wtoshm <wtoshm@gmail.com>
2025-06-04 15:22:35 +0900

    Add selfAid method to basic Cleric class

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -6,4 +6,13 @@ class Cleric {
   final int maxMp = 10;
 
   Cleric(this.name);
+
+  void selfAid() {
+    if(mp >= 5) {
+      mp = mp - 5;
+      hp = maxHp;
+    } else {
+      print("self aid 에는 최소한 5 mp가 필요합니다.");
+    }
+  }
 }
