da595049c7108504b38f83770d2dbad06e8bcb29
SibangGyeon <briankim0695@gmail.com>
2025-06-04 16:30:48 +0900

    feat: 연습문제 3 Cleric pray() 추가 및 테스트 코드 작성

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name;
   int hp;
@@ -11,6 +13,29 @@ class Cleric {
     if (this.mp >= 5) {
       this.mp -= 5;
       this.hp = this.maxHp;
+    }
+  }
+
+  int pray(int time) {
+    // 기도 시간은 최대 초당 1씩 회복 된다는 기준
+    // 기도는 mp가 최대가 아닐 경우만 가능
+    if (((time > 0) && (time <= this.maxMp)) && this.mp < this.maxMp) {
+      int previousMp = this.mp;
+      int recoveryMp;
+
+      // 회복 보정 0 ~ 2
+      this.mp += time + Random().nextInt(3);
+
+      // maxMp 초과 회복 불가
+      if (this.mp > this.maxMp) {
+        this.mp = this.maxMp;
+      }
+
+      recoveryMp = this.mp - previousMp;
+
+      return recoveryMp;
+    } else {
+      return 0;
     }
   }
 }
--- test/cleric_test.dart
+++ test/cleric_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:modu_3_dart_study/cleric.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
@@ -11,7 +13,6 @@ void main() {
     expect(cleric.maxHp, equals(50));
     expect(cleric.mp, equals(10));
     expect(cleric.maxMp, equals(10));
-
   });
 
   test('Cleric Self Aid Test', () {
@@ -28,5 +29,22 @@ void main() {
     // 자힐 불가능 (mp가 5미만)
     expect(cleric2.mp < 5, equals(true));
     expect(cleric2.hp == cleric2.maxHp, equals(false));
+  });
+
+  test('Cleric Pray Test', () {
+    // mp가 최대가 아닌 성직자
+    Cleric cleric1 = new Cleric('성직자', 50, 50, 0, 10);
+    Cleric cleric2 = new Cleric('성직자', 50, 50, 9, 10);
+
+    // mp가 최대인 성직자
+    Cleric cleric3 = new Cleric('성직자', 50, 50, 10, 10);
+
+    int recoveryMp1 = cleric1.pray(Random().nextInt(cleric1.maxMp + 1));
+    int recoveryMp2 = cleric2.pray(1);
+    int recoveryMp3 = cleric3.pray(1);
+
+    expect(recoveryMp1 >= 1 && recoveryMp1 <= 10, equals(true));
+    expect(recoveryMp2, equals(1));
+    expect(recoveryMp3, equals(0));
   });
 }
