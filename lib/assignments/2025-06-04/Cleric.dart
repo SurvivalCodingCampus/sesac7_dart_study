7882f18a095397464e4a25b72da35b386278a309
2kihyuk <kihyuk5566@naver.com>
2025-06-04 17:08:58 +0900

    feat : pray 메소드 예외 처리

--- lib/assignments/2025-06-04/Cleric.dart
+++ lib/assignments/2025-06-04/Cleric.dart
@@ -11,21 +11,36 @@ class Cleric {
   Cleric(this.name);
 
   void selfAid(){
+    print('회복 이전 hp : $hp');
     mp-=5;
     hp = max_hp;
+    print('현재 mp : $mp , 회복 이후 hp : $hp');
   }
 
   int pray(int second){
+    //이미 최대 mp면 0 리턴.
+    if(mp == max_mp){
+      return 0;
+    }
+
     int beforeMp = mp;
     int heal = second + Random().nextInt(3); //회복량
 
+    //회복하기전 mp와 회복량, 즉 회복 이후의 mp값이 최대 mp보다 크다면,
+    //mp = max_mp  이고 max_mp에서 회복 이전 값을 뺀 값을 반환(회복량)
     if(beforeMp + heal >= max_mp){
       mp = max_mp;
+      print('회복 이후 mp : $mp , 회복량 : ${max_mp - beforeMp}');
       return max_mp - beforeMp;
     }else{
+      //회복이후의 mp값이 최대 mp값 보다 작다면 ,
+      //mp+= 회복량 이고 회복이후의 mp에서 회복 이전의 mp를 뺀 값을 반환(회복량)
       mp+=heal;
+      print('회복 이후 mp : $mp , 회복량 : ${mp - beforeMp}');
       return mp - beforeMp;
     }
   }
 
 }
+
+
--- test/assignments/2025-06-04/Cleric_test.dart
+++ test/assignments/2025-06-04/Cleric_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:modu_3_dart_study/assignments/2025-06-04/Cleric.dart';
 import 'package:test/test.dart';
 
@@ -6,14 +8,34 @@ void main(){
   test('Cleric Test', (){
     Cleric cleric = Cleric('성직자');
     expect(cleric.name, '성직자');
+    expect(cleric.hp == 50, true);
+    expect(cleric.max_hp, equals(50));
+    expect(cleric.mp , 10);
+    expect(cleric.max_mp , isNot(49));
 
+    cleric.hp = 30;
+    
     cleric.selfAid();
     //mp 5 소비
     expect(cleric.mp ==5, true);
 
-    cleric.pray(3);
-    // 3초간 Mp 회복
+    cleric.hp = 0;
+    cleric.selfAid();
+    //mp 5소비
+    expect(cleric.mp ,equals(0));
 
-    expect(cleric.mp <=10, true);
+    cleric.pray(3);
+    // 3초간 mp 회복
+    //mp 0에서 3초 회복해서, 5이하의 mp가 충전됨.
+    expect(cleric.mp <=5, true);
+
+    //1초간 mp 회복
+    cleric.pray(1);
+    // 최대 mp 5에서 1초 회복해서, 최대치가 8
+    expect(cleric.mp <=8, true);
+
   });
+
+
+
 }
