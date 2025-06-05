239cc8c3ce9a890ae568f8a5846b4b216ee46213
Hagmin.Lee <iilhm2001@naver.com>
2025-06-05 16:08:51 +0900

    feat: 최대 HP, MP 필드 수정 및 매직넘버 제거

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -5,36 +5,36 @@ class Cleric {
   String name; // 이름
   int hp = 50; // hp
   int mp = 10; // mp
-  final int maxHp = 50; // 최대 hp
-  final int maxMp = 10; // 최대 mp
+  final int prayCorrectionRange = 3; // 기도 마법 회복 보정 범위
+  static final int maxHp = 50; // 최대 hp
+  static final int maxMp = 10; // 최대 mp
 
-  // method
   Cleric(this.name); // 생성자, 성직자 이름 초기화
 
+  // method
   void selfAid() {
-    if(mp >= 5) { // 소비할 mp가 충분할 때만(5 이상) 마법 사용
+    if (mp < 5) {
+      print('mp가 충분하지 않습니다!');
+      return;
+    } else {
       mp -= 5;
       hp = maxHp;
-    }
-    else {
-      print('mp가 충분하지 않습니다!');
     }
   }
 
   int pray(int second) {
     int result = 0; // 실질 회복량. 0초 기도를 해도 값이 나올 수 있도록 0으로 초기화.
 
-    if(second > 0) { // 기도 시간이 양수일 때만 실질적인 마법 사용.
-      final int mpGen = second + Random().nextInt(3); // mp 회복 전체양(초과분도 포함), 기도 시간 + 0~2 랜덤 정수
+    if (second > 0) { // 기도 시간이 양수일 때만 실질적인 마법 사용.
+      final int mpRecoveryAmount = second + Random().nextInt(prayCorrectionRange); // mp 회복 전체양(초과분도 포함), 기도 시간 + 0~2 랜덤 정수
 
-      if(mp + mpGen <= 10) { // 10까지는 mpGen 값이 그대로 회복량이 됨.
-        mp += mpGen;
-        result = mpGen;
-      }
-      else { // 회복했을 때 10을 넘어갈 경우
-        final int temp = mp; // 회복 전 mp 값을 임시로 저장하고,
+      if (mp + mpRecoveryAmount <= maxMp) { // 10까지는 mpRecoveryAmount 값이 그대로 회복량이 됨.
+        mp += mpRecoveryAmount;
+        result = mpRecoveryAmount;
+      } else { // 회복했을 때 10을 넘어갈 경우
+        final int mpBeforeRecovery = mp; // 회복 전 mp 값을 임시로 저장하고,
         mp = maxMp; // mp를 최대치까지 회복한 후,
-        result = maxMp - temp; // mp 최대치(10) - 회복 전 mp 값(temp)을 반환.
+        result = maxMp - mpBeforeRecovery; // mp 최대치(10) - 회복 전 mp 값(mpBeforeRecovery)을 반환.
       }
     }
 
