6ff6230293a09d25cf52707a7f299d77e15a19c8
2kihyuk <kihyuk5566@naver.com>
2025-06-05 15:40:06 +0900

    chore : cleric 클래스 새로 생성

--- test/assignments/2025-06-04/Cleric_test.dart
+++ test/assignments/2025-06-04/Cleric_test.dart
@@ -2,9 +2,7 @@ import 'package:modu_3_dart_study/assignments/2025-06-04/Cleric.dart';
 import 'package:test/test.dart';
 
 void main() {
-
   group('Cleric Test', () {
-
     test('생성자 테스트(초기상태 확인)', () {
       Cleric cleric = Cleric('성직자');
       expect(cleric.name, '성직자');
@@ -13,21 +11,21 @@ void main() {
       expect(cleric.mp, 10);
       expect(cleric.maxMp, 10);
     });
-    
-    test('selfAid 테스트',(){
+
+    test('selfAid 테스트', () {
       Cleric cleric = Cleric('성직자');
       cleric.hp = 30;
       cleric.selfAid();
       expect(cleric.mp, 5);
       expect(cleric.mp, equals(5));
 
-      cleric.hp =0;
+      cleric.hp = 0;
       cleric.selfAid();
       expect(cleric.mp, 0);
       expect(cleric.mp == 0, true);
     });
 
-    test('pray 테스트', (){
+    test('pray 테스트', () {
       Cleric cleric = Cleric('성직자');
       cleric.mp = 0;
       cleric.pray(3);
@@ -36,6 +34,5 @@ void main() {
       cleric.pray(1);
       expect(cleric.mp <= 8, true);
     });
-    
   });
 }
