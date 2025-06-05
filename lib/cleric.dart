1ebd72f4ccb5a5a705d6378cf26a437905390a83
wtoshm <wtoshm@gmail.com>
2025-06-05 16:43:52 +0900

    Refactor Cleric class by deleting commented codes

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -1,14 +1,6 @@
 import 'dart:math';
 
 class Cleric {
-  // String name;
-  // int hp = 50;
-  // final int maxHp = 50;
-  // int mp = 10;
-  // final int maxMp = 10;
-  //
-  // Cleric(this.name);
-
   static const int maxHp = 50;
   static const int maxMp = 10;
 
@@ -19,13 +11,6 @@ class Cleric {
   Cleric(this.name, {this.hp = maxHp, this.mp = maxMp});
 
   void selfAid() {
-    // if (mp >= 5) {
-    //   mp = mp - 5;
-    //   hp = maxHp;
-    // } else {
-    //   print("self aid 에는 최소한 5 mp가 필요합니다.");
-    // }
-
     // self-aid 에 따라 차감되는 mp 양 - 5 mp
     final mpNecessary = 5;
 
@@ -40,37 +25,12 @@ class Cleric {
     hp = maxHp;
   }
 
-  // int pray(int prayTime) {
-  //   int mpPrayPoint = 0;
-  //   if (prayTime > 0) {
-  //     mpPrayPoint = prayTime + Random().nextInt(3);
-  //   } else {
-  //     print(" pray 시간은 1초 이상이어야 합니다.");
-  //   }
-  //
-  //   int maxMpCanBeRecovered = maxMp - mp;
-  //
-  //   if(mpPrayPoint > maxMpCanBeRecovered) {
-  //     mp = mp + maxMpCanBeRecovered;
-  //     return maxMpCanBeRecovered;
-  //   } else {
-  //     mp = mp + mpPrayPoint;
-  //     return mpPrayPoint;
-  //   }
-  // }
-
   int pray(int praySeconds) {
     // 리턴될 mpPointsRecovered 정의 및 계산
     int mpPointsRecovered = 0;
 
     // 회복되는 +alpha 값의 최대치
     final maxPlusAlphaRecovered = 2;
-
-    // if (praySeconds > 0) {
-    //   mpPointsRecovered = praySeconds + Random().nextInt(3);
-    // } else {
-    //   print("pray 시간은 1초이상이어야 합니다.");
-    // }
 
     if (praySeconds <= 0) {
       print("pray 시간은 1초이상이어야 합니다.");
@@ -82,12 +42,6 @@ class Cleric {
 
     // 최대 mpPointsRecovered 정의 및 계산
     int maxMpPointsRecovered = maxMp - mp;
-
-    // // mpPointsRecovered 가 최대 maxMpPointsRecovered 보다 클 경우
-    // // mpPointsRecovered 를 최대 maxMpPointsRecovered 로 재할당
-    // if (mpPointsRecovered > maxMpPointsRecovered) {
-    //   mpPointsRecovered = maxMpPointsRecovered;
-    // }
 
     // maxMpPointsRecovered 제약 체크
     mpPointsRecovered = min(mpPointsRecovered, maxMpPointsRecovered);
