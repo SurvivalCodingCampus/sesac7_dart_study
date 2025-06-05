0c2d0c9efcab9eb6273f73c2a1251d957df54459
ParkGiTeak <rl980901@naver.com>
2025-06-05 15:39:44 +0900

    fix: Cleric Class maxHp, maxMp static변수로 수정 및 과제 피드백 수정

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -1,21 +1,24 @@
 import 'dart:math';
 
 class Cleric {
+  static int maxHp = 50;
+  static int maxMp = 10;
+
   String name;
   int hp = maxHp;
-  static const int maxHp = 50;
   int mp = maxMp;
-  static const int maxMp = 10;
+
+  final selfAidUseMp = 5;
 
   Cleric(this.name);
 
   void selfAid() {
-    if (mp >= 5) {
-      mp -= 5;
-      hp = maxHp;
-    } else {
-      print('mp 소모 불가능');
+    if (mp < selfAidUseMp) {
+      print('mp 부족');
+      return;
     }
+    mp -= selfAidUseMp;
+    hp = maxHp;
   }
 
   int pray(int time) {
