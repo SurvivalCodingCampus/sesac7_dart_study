b34c2723ab2aebac4383e7739884224eb4e3f5c4
0h9u <kog6148@gmail.com>
2025-06-11 18:34:45 +0900

    feat : wand 생성자, wizard 생성자 getter, setter 추가

--- lib/wand.dart
+++ lib/wand.dart
@@ -1,6 +1,27 @@
 class Wand {
-  String name;
-  double power;
+  String _name;
+  double _power;
 
-  Wand({required this.name, required this.power});
+  Wand(this._name, this._power) {
+    name = _name;
+    power = _power;
+  }
+
+  String get name => _name;
+
+  double get power => _power;
+
+  set name(String newName) {
+    if (newName.length < 3) {
+      throw Exception('에러 : 지팡이 이름은 3자 이상');
+    }
+    _name = newName;
+  }
+
+  set power(double newPower) {
+    if (newPower < 0.5 || newPower > 100.0) {
+      throw Exception('에러 : 지팡이 마력은 0.5 이상 100.0 이하');
+    }
+    _power = newPower;
+  }
 }
