4439bbcc71e4ae8ca7b645b2a18162788da2d3c8
0h9u <kog6148@gmail.com>
2025-06-11 19:45:36 +0900

    feat : person 생성자, person 나이 계산 추가

---
+++ lib/assignments/person.dart
@@ -0,0 +1,12 @@
+class Person {
+  final String _name;
+  final int _birthYear;
+
+  Person(this._name, this._birthYear);
+
+  String get name => _name;
+
+  int get birthYear => _birthYear;
+
+  int get age => DateTime.now().year - _birthYear;
+}
