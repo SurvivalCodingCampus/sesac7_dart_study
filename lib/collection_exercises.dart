0119abe9cca8e67777599e7e7a0a4a8632975517
wtoshm <wtoshm@gmail.com>
2025-06-11 16:11:35 +0900

    Add collection exercise 3 (홍길동, 한석봉 List of Persons)

--- lib/collection_exercises.dart
+++ lib/collection_exercises.dart
@@ -2,6 +2,22 @@ void main() {
   people.entries.forEach((person) {
     print('${person.key}의 나이는 ${person.value}살');
   });
+
+  print('---------------------------');
+
+  for (final person in peopleList) {
+    print(person.name);
+  }
 }
 
 Map<String, int> people = {'홍길동': 20, '한석봉': 25};
+
+// ----------------------------
+
+class Person {
+  final String name;
+
+  Person(this.name);
+}
+
+List<Person> peopleList = [Person('홍길동'), Person('한석봉')];
