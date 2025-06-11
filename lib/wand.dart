bb1c0b4ba261d7f524ee937dc41b8eb2bcbc1a88
ParkGiTeak <rl980901@naver.com>
2025-06-11 18:45:53 +0900

    fix: Wand Class wandNameLimit, wandPowerMinValue, wandPowerMaxValue public으로 수정

--- lib/wand.dart
+++ lib/wand.dart
@@ -2,16 +2,16 @@ class Wand {
   String _name;
   double _power;
 
-  final int _wandNameLimit = 3;
-  final double _wandPowerMinValue = 0.5;
-  final double _wandPowerMaxValue = 100.0;
+  final int wandNameLimit = 3;
+  final double wandPowerMinValue = 0.5;
+  final double wandPowerMaxValue = 100.0;
 
   Wand({required String name, required double power}): _name = name, _power = power;
 
   String get wandName => _name;
   set wandName(String value) => {
-    if (value.length < _wandNameLimit) {
-      throw Exception('wnadName은 $_wandNameLimit문자 이상이어야 합니다.')
+    if (value.length < wandNameLimit) {
+      throw Exception('wnadName은 $wandNameLimit문자 이상이어야 합니다.')
     }
     else {
       _name = value
@@ -20,8 +20,8 @@ class Wand {
 
   double get wandPower => _power;
   set wandPower(double value) => {
-    if (value < _wandPowerMinValue || value > _wandPowerMaxValue) {
-      throw Exception('wandPower는 $_wandPowerMinValue 이상 $_wandPowerMaxValue 이하로 설정해야 합니다.')
+    if (value < wandPowerMinValue || value > wandPowerMaxValue) {
+      throw Exception('wandPower는 $wandPowerMinValue 이상 $wandPowerMaxValue 이하로 설정해야 합니다.')
     } else {
       _power = value
     }
