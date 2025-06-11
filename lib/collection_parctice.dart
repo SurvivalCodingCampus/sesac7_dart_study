a9f494f8195f7ad789aab7c1963ceaef291a5ec7
SibangGyeon <briankim0695@gmail.com>
2025-06-11 19:19:20 +0900

    feat: 컬렉션 연습문제 3번 구현

--- lib/collection_parctice.dart
+++ lib/collection_parctice.dart
@@ -2,6 +2,7 @@ import 'package:modu_3_dart_study/person.dart';
 
 void main() {
   practice2();
+  practice3();
 }
 
 void practice2() {
@@ -9,5 +10,15 @@ void practice2() {
 
   for (var element in people.entries) {
     print('"${element.key}의 나이는 ${element.value}살"');
+  }
+}
+
+void practice3() {
+  final person1 = Person(name: '홍길동', birthYear: 1999);
+  final person2 = Person(name: '한석봉', birthYear: 1999);
+  List<Person> people = [person1, person2];
+
+  for (var person in people) {
+    print(person.name);
   }
 }
