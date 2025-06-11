391bc4e1fe59467cecd91a809683d94d60570ef4
ParkGiTeak <rl980901@naver.com>
2025-06-11 16:47:14 +0900

    feat: 컬렉션 연습문제 2 - 컬렉션의 응용 문제

--- lib/person.dart
+++ lib/person.dart
@@ -6,3 +6,17 @@ class Person {
 
   int get age => DateTime.now().year - birthYear;
 }
+
+void _collectionPractice2() {
+  List<Map<String, dynamic>> people = [];
+  people.add({'name': '홍길동', 'age': 20});
+  people.add({'name': '한석봉', 'age': 25});
+
+  for (Map<String, dynamic> person in people) {
+    print('${person['name']}의 나이는 ${person['age']}살');
+  }
+}
+
+void main() {
+  _collectionPractice2();
+}
