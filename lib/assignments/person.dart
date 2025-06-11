c1650edf3fff6d53bb34d06da7ff432a81ced595
0h9u <kog6148@gmail.com>
2025-06-11 20:29:45 +0900

    feat : 한석봉, 홍길동 collection 저장 후 출력

--- lib/assignments/person.dart
+++ lib/assignments/person.dart
@@ -10,3 +10,11 @@ class Person {
 
   int get age => DateTime.now().year - _birthYear;
 }
+
+void main() {
+  final Map<String, dynamic> honggildong = {'name': '홍길동', 'age': 20};
+  final Map<String, dynamic> hanseokbong = {'name': '한석봉', 'age': 25};
+
+  print('${honggildong['name']}의 나이는 ${honggildong['age']}살');
+  print('${hanseokbong['name']}의 나이는 ${hanseokbong['age']}살');
+}
