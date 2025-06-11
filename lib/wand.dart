9f4c7c1b83fb51a90263e4af07abec6e25f2d351
ParkGiTeak <rl980901@naver.com>
2025-06-11 15:43:10 +0900

    feat: Wizard, Wand Class property getter 추가 및 setter 타당성 검사 예외처리 추가

--- lib/wand.dart
+++ lib/wand.dart
@@ -1,6 +1,29 @@
 class Wand {
-  String name;
-  double power;
+  String _name;
+  double _power;
 
-  Wand({required this.name, required this.power});
+  final int _wandNameLimit = 3;
+  final double _wandPowerMinValue = 0.5;
+  final double _wandPowerMaxValue = 100.0;
+
+  Wand({required String name, required double power}): _name = name, _power = power;
+
+  String get wandName => _name;
+  set wandName(String value) => {
+    if (value.length < _wandNameLimit) {
+      throw Exception('wnadName은 $_wandNameLimit문자 이상이어야 합니다.')
+    }
+    else {
+      _name = value
+    }
+  };
+
+  double get wandPower => _power;
+  set wandPower(double value) => {
+    if (value < _wandPowerMinValue || value > _wandPowerMaxValue) {
+      throw Exception('wandPower는 $_wandPowerMinValue 이상 $_wandPowerMaxValue 이하로 설정해야 합니다.')
+    } else {
+      _power = value
+    }
+  };
 }
--- lib/wizard.dart
+++ lib/wizard.dart
@@ -1,10 +1,53 @@
 import 'package:modu_3_dart_study/wand.dart';
 
 class Wizard {
-  String name;
-  int hp;
-  int mp;
-  Wand? wand;
+  String _name;
+  int _hp;
+  int _mp;
+  Wand? _wand;
 
-  Wizard(this.name, {required this.hp, required this.mp, this.wand});
+  final int _wizardNameLimit = 3;
+  final int _wizardMinMpLimit = 0;
+
+  Wizard({required String name, required int hp, required int mp, Wand? wand}): _name = name, _hp = hp, _mp = mp;
+
+  String get wizardName => _name;
+  set wizardName(String value) => {
+    if (value.length < _wizardNameLimit) {
+      throw Exception('wizardName은 $_wizardNameLimit문자 이상이어야 합니다.')
+    } else {
+      _name = value
+    }
+  };
+
+  int get wizardHp => _hp;
+  set wizardHp(int value) => {
+    if (value < 0) { // hp set value 값이 음수인 경우
+      _hp = 0 // 음수 대신 0 으로 저장
+    } else {
+      _hp = value
+    }
+  };
+
+  int get wizardMp => _mp;
+  set wizardMp(int value) => {
+    if (value < _wizardMinMpLimit) {
+      throw Exception('WizardMp 값은 항상 $_wizardMinMpLimit 이상이어야 합니다')
+    } else {
+      _mp = value
+    }
+  };
+
+  /*
+  * _wand property가 nullable하게 타입이 지정되어 있고
+  * setter에서 notNull로 설정하면 getter의 return type을 nullable하게 할 수 없다.
+  */
+  Wand? get wand => _wand;
+  set wand(Wand? value) => {
+    if (wand == null) {
+      throw Exception('Wizard 생성 후 wand value를 null로 설정할 수 없습니다.')
+    } else {
+      _wand = value
+    }
+  };
 }
