0d795fb8d65e4fa651b38327cd528a61bc659463
Hagmin.Lee <iilhm2001@naver.com>
2025-06-13 02:29:33 +0900

    test: PoisonSlime 테스트 코드 수정(독 살포 후 상태에 대한 검증 추가)

--- test/poison_slime_test.dart
+++ test/poison_slime_test.dart
@@ -20,6 +20,7 @@ void main() {
       // given
       final int poisonCountInit = 5; // 독 살포 횟수 최초 초기화 값(고정)
       final int testHp = 1000; // 용사가 죽는 것을 방지하기 위해 임의의 매우 큰 값 제공
+      final int dieHp = 0;
       final PoisonSlime poisonSlime1 = PoisonSlime('레드 슬라임');
       final PoisonSlime poisonSlime2 = PoisonSlime('블루 슬라임');
       final PoisonSlime poisonSlime3 = PoisonSlime('그린 슬라임');
@@ -38,17 +39,26 @@ void main() {
       for(int i = 0; i < testAttackCount1; i++) {
         expect(() => poisonSlime1.attack(hero1), returnsNormally);
       }
+      expect(poisonSlime1.poisonCount, equals(poisonCountInit - testAttackCount1));
+      expect(hero1.hp, lessThan(testHp));
+      expect(hero1.hp, greaterThan(dieHp));
 
       expect(poisonSlime2.poisonCount, equals(poisonCountInit));
       for(int i = 0; i < testAttackCount2; i++) {
         expect(() => poisonSlime2.attack(hero2), returnsNormally);
       }
+      expect(poisonSlime1.poisonCount, equals(poisonCountInit - testAttackCount1));
+      expect(hero1.hp, lessThan(testHp));
+      expect(hero1.hp, greaterThan(dieHp));
 
       // 공격 횟수가 6번인 경우 5번은 독을 살포하고 마지막엔 독 살포 횟수가 부족하여 살포하지 않는다.
       expect(poisonSlime3.poisonCount, equals(poisonCountInit));
       for(int i = 0; i < testAttackCount3; i++) {
         expect(() => poisonSlime3.attack(hero3), returnsNormally);
       }
+      expect(poisonSlime1.poisonCount, equals(poisonCountInit - testAttackCount1));
+      expect(hero1.hp, lessThan(testHp));
+      expect(hero1.hp, greaterThan(dieHp));
     });
 
     test('용사가 이미 죽었다면 공격을 하지 않는다', () {
