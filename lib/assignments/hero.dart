82889fa03048624ae8ba213dcaa348d4da044a50
0h9u <kog6148@gmail.com>
2025-06-12 20:49:05 +0900

    feat : hero, slime 생성자 추가

---
+++ lib/assignments/hero.dart
@@ -0,0 +1,16 @@
+import '../slime.dart';
+
+class Hero {
+  String name;
+  int hp;
+
+  Hero(this.name, this.hp);
+
+  void attack(Slime slime) {
+    print('${name}이 ${slime}을 공격했다.');
+  }
+
+  void run() {
+    print('${name}이 도망쳤다.');
+  }
+}
