5a39cad1f00645daeaf31308982ff97e49228daa
Hagmin.Lee <iilhm2001@naver.com>
2025-06-12 21:04:15 +0900

    test: Hero 클래스 테스트 코드 수정(테스트 케이스 추가)

--- test/hero_test.dart
+++ test/hero_test.dart
@@ -2,14 +2,44 @@ import 'package:modu_3_dart_study/hero.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Hero Test', () {
-    // given
-    Hero hero = Hero('홍길동', 100);
+  group('Hero Test', () {
+    test('생성자 테스트', () {
+      // given
+      final int testHp = 100;
+      final Hero hero = Hero(name: '이학민', hp: testHp);
 
-    // when
-    hero.attack();
+      // when & then
+      expect(hero.name, equals('이학민'));
+      expect(hero.hp, equals(testHp));
+      expect(hero.hpMax, equals(testHp)); // 입력으로 받은 hp 값이 곧 hp 최대치로 설정
+    });
 
-    // then
-    expect(hero.hp, equals(99));
+    test('setter 동작 여부 확인', () {
+      // given
+      final int testHp = 100;
+      final int testInputHp = 200;
+      final Hero hero = Hero(name: '이학민', hp: testHp);
+
+      // when
+      // hpMax는 final로 선언된 상수라 변경 불가능
+      hero.name = '홍길동';
+      hero.hp = testInputHp;
+
+      // then
+      expect(hero.name, equals('홍길동'));
+      expect(hero.hp, equals(testInputHp));
+    });
+
+    test('각 메소드 동작 여부 확인', () {
+      // given
+      final int testHp = 100;
+      final Hero hero = Hero(name: '이학민', hp: testHp);
+
+      // when & then
+      expect(() => hero.attack(), returnsNormally);
+      expect(() => hero.run(), returnsNormally);
+    });
   });
+
+
 }
