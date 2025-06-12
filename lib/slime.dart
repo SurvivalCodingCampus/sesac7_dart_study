c77f2ccdef03cc52b256c0b0306981be6ab638e3
ParkGiTeak <rl980901@naver.com>
2025-06-12 14:52:51 +0900

    fix: Slime Class 상속 연습문제 3번과 동일하게 필드 수정

--- lib/slime.dart
+++ lib/slime.dart
@@ -1,12 +1,20 @@
+import 'package:modu_3_dart_study/hero.dart';
+
 class Slime {
   final int level = 10;
 
-  int hp;
-  String suffix;
+  int hp = 50;
+  final String suffix;
 
-  Slime(this.hp, this.suffix);
+  Slime(this.suffix);
 
   void run() {
     print('슬라임 $suffix가 도망갔다.');
+  }
+
+  void attack(Hero hero) {
+    print('슬라임$suffix이/가 공격했다.');
+    print('10의 데미지');
+    hero.hp -= 10;
   }
 }
