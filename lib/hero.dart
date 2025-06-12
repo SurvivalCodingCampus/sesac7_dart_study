90010a28b660ea328b997bc409069c75a5d5726f
SibangGyeon <briankim0695@gmail.com>
2025-06-12 18:45:45 +0900

    feat: Hero 클래스 maxHp 추가

--- lib/hero.dart
+++ lib/hero.dart
@@ -1,10 +1,12 @@
 class Hero {
   String name;
   int _hp; // private 필드
+  final int _maxHp;
 
-  int get hp => _hp; // hp라는 프러퍼티가 만들어짐
+  int get hp => _hp; // hp라는 프로퍼티가 만들어짐
+  int get maxHp => _maxHp;
 
-  Hero({required this.name, int hp = 100}) : _hp = hp;
+  Hero({required this.name, int hp = 100}) : _maxHp = hp, _hp = hp; // 뭐지?
 
   set hp(int value) {
     _hp = value;
