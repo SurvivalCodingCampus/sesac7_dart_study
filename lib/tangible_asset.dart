27a75a6f83d16f8400f46b1c7695801103e2438f
Hagmin.Lee <iilhm2001@naver.com>
2025-06-13 16:11:37 +0900

    feat: TangibleAsset 클래스에 Thing 인터페이스 implements 및 메소드 오버라이드

--- lib/tangible_asset.dart
+++ lib/tangible_asset.dart
@@ -1,10 +1,22 @@
 import 'package:modu_3_dart_study/asset.dart';
+import 'package:modu_3_dart_study/thing.dart';
 
-abstract class TangibleAsset extends Asset {
+abstract class TangibleAsset extends Asset implements Thing {
+  double _weight;
+
+  // getter 오버라이드
+  @override
+  double get weight => _weight;
+
+  // setter 오버라이드
+  @override
+  set weight(double weight) => _weight = weight;
+
   // 생성자
   TangibleAsset({
     required super.name,
     required super.price,
     required super.color,
-  });
+    required double weight,
+  }) : _weight = weight;
 }
--- lib/thing.dart
+++ lib/thing.dart
@@ -1,4 +1,8 @@
 abstract interface class Thing {
+  // getter, setter가 메소드를 통해 필드에 접근할 수 있도록 도와주고,
+  // 프로퍼티의 경우에도 결국 getter, setter의 기능을 캡슐화 한 것이기 때문에,
+  // 따라서 필드가 존재하지 않아야 하는 인터페이스 내부에 있어도 괜찮은 것.
+  
   // getter
   double get weight;
 
