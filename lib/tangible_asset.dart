7b4d97e16ef81a74f61481fc5b8b529468c48108
SibangGyeon <briankim0695@gmail.com>
2025-06-13 21:15:30 +0900

    refactor: TangibleAsset 수정

--- lib/tangible_asset.dart
+++ lib/tangible_asset.dart
@@ -3,22 +3,32 @@ import 'package:modu_3_dart_study/thing.dart';
 
 abstract class TangibleAsset extends Asset implements Thing {
   String _color;
-
-  @override
-  double weight;
+  double _weight;
 
   String get color => _color;
+
+  @override
+  double get weight => _weight;
 
   TangibleAsset({
     required super.name,
     required super.price,
     required String color,
-    required this.weight,
-  }) : _color = color;
+    required double weight,
+  }) : _color = color,
+       _weight = weight;
 
   set color(String value) {
     if (value.isNotEmpty) {
       _color = value;
     }
     _color = '';
+  }
+
+  @override
+  set weight(double value) {
+    if (value > 0) {
+      _weight = value;
+    }
+    _weight = 0.0;
 }
