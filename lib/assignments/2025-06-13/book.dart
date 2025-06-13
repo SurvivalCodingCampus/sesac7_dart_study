5494d9141a7bd1d8a0f56268c1616f7bd6b956d1
Sumin Park <suminp12@gmail.com>
2025-06-13 14:30:03 +0900

    feat: Book and Computer class added

---
+++ lib/assignments/2025-06-13/book.dart
@@ -0,0 +1,8 @@
+class Book {
+  String name;
+  int price;
+  String color;
+  String isbn;
+
+  Book(this.name, this.price, this.color, this.isbn);
+}
---
+++ lib/assignments/2025-06-13/computer.dart
@@ -0,0 +1,8 @@
+class Computer {
+  String name;
+  int price;
+  String color;
+  String makerName;
+
+  Computer(this.name, this.price, this.color, this.makerName);
+}
