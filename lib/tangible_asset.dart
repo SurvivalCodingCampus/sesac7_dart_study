712dfde32c76db53253daa3a51091bcdfcd62584
SibangGyeon <briankim0695@gmail.com>
2025-06-13 16:30:55 +0900

    refactor: TangibleAsset에서 weight 오버라이드 추가

--- lib/tangible_asset.dart
+++ lib/tangible_asset.dart
@@ -4,11 +4,15 @@ import 'package:modu_3_dart_study/thing.dart';
 abstract class TangibleAsset extends Asset implements Thing {
   final String _color;
 
+  @override
+  double weight;
+
   String get color => _color;
 
   TangibleAsset({
     required super.name,
     required super.price,
     required String color,
+    required this.weight,
   }) : _color = color;
 }
