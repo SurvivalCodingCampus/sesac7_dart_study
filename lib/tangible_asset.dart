39d5d4957abf9725298e749c9f8ff290e29e7737
wtoshm <wtoshm@gmail.com>
2025-06-13 16:29:47 +0900

    refactor: TangibleAsset class 수정 - Thing interface 구현

--- lib/tangible_asset.dart
+++ lib/tangible_asset.dart
@@ -1,11 +1,16 @@
 import 'package:modu_3_dart_study/asset.dart';
+import 'package:modu_3_dart_study/thing.dart';
 
-abstract class TangibleAsset extends Asset {
+abstract class TangibleAsset extends Asset implements Thing {
   String color;
+
+  @override
+  double weight;
 
   TangibleAsset({
     required super.name,
     required super.price,
     required this.color,
+    required this.weight,
   });
 }
