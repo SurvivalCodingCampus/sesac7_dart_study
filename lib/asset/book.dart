f6b41dbb3c0355a73d662b610ce3dbcd0267b6eb
ParkGiTeak <rl980901@naver.com>
2025-06-13 17:32:32 +0900

    feat: Book Class, Computer Class 추가

--- lib/asset/book.dart
+++ lib/asset/book.dart
@@ -1,12 +1,38 @@
-import 'package:modu_3_dart_study/asset/tanglble_asset.dart';
+import 'package:modu_3_dart_study/asset/tangible_asset.dart';
 
 class Book extends TangibleAsset {
   String isbn;
+
+  @override
+  double weight;
+
+  @override
+  double tangibleAssetDepth;
+
+  @override
+  double tangibleAssetHeight;
+
+  @override
+  double tangibleAssetWidth;
 
   Book({
     required super.name,
     required super.price,
     required super.color,
+    required super.texture,
     required this.isbn,
+    required this.weight,
+    required this.tangibleAssetWidth,
+    required this.tangibleAssetHeight,
+    required this.tangibleAssetDepth,
   });
+
+  @override
+  void destroy() {
+    super.destroy();
+    weight = 0.0;
+    tangibleAssetWidth = 0.0;
+    tangibleAssetHeight = 0.0;
+    tangibleAssetDepth = 0.0;
+  }
 }
--- lib/asset/computer.dart
+++ lib/asset/computer.dart
@@ -1,12 +1,29 @@
-import 'package:modu_3_dart_study/asset/tanglble_asset.dart';
+import 'package:modu_3_dart_study/asset/tangible_asset.dart';
 
 class Computer extends TangibleAsset {
   String makerName;
+
+  @override
+  double weight;
+
+  @override
+  double tangibleAssetDepth;
+
+  @override
+  double tangibleAssetHeight;
+
+  @override
+  double tangibleAssetWidth;
 
   Computer({
     required super.name,
     required super.price,
     required super.color,
+    required super.texture,
     required this.makerName,
+    required this.weight,
+    required this.tangibleAssetWidth,
+    required this.tangibleAssetHeight,
+    required this.tangibleAssetDepth,
   });
 }
