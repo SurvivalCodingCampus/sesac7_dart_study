4b8bd1aaa623f5e7733162c5e8c8d64669c6fe88
Hagmin.Lee <iilhm2001@naver.com>
2025-06-13 14:28:40 +0900

    feat: Book, Computer 클래스 생성

--- lib/book.dart
+++ lib/book.dart
@@ -1,1 +1,17 @@
-class Book {}
+import 'package:modu_3_dart_study/tangible_asset.dart';
+
+class Book extends TangibleAsset {
+  String _isbn;
+
+  // getter
+  String get isbn => _isbn;
+
+  // 필요한 경우 setter 추가
+
+  Book({
+    required super.name,
+    required super.price,
+    required super.color,
+    required String isbn,
+  }) : _isbn = isbn;
+}
--- lib/computer.dart
+++ lib/computer.dart
@@ -1,1 +1,17 @@
-class Computer {}
+import 'package:modu_3_dart_study/tangible_asset.dart';
+
+class Computer extends TangibleAsset {
+  String _makerName;
+
+  // getter
+  String get makerName => _makerName;
+
+  // 필요한 경우 setter 추가
+
+  Computer({
+    required super.name,
+    required super.price,
+    required super.color,
+    required String makerName,
+  }) : _makerName = makerName;
+}
