03226dca2889c7931b290cbff6260b126610cda9
0h9u <kog6148@gmail.com>
2025-06-13 19:50:22 +0900

    feat : tangible 추상 클래스 thing 추가 weight 속성 추가

--- lib/assignments/tangible_asset.dart
+++ lib/assignments/tangible_asset.dart
@@ -1,7 +1,24 @@
 import 'package:modu_3_dart_study/assignments/asset.dart';
+import 'package:modu_3_dart_study/assignments/thing.dart';
 
-abstract class TangibleAsset extends Asset {
-  String color;
+abstract class TangibleAsset extends Asset implements Thing {
+  final String color;
+  double _weight;
 
-  TangibleAsset(super.name, super.price, this.color);
+  @override
+  double get weight {
+    return _weight;
+  }
+
+  @override
+  set weight(double value) {
+    _weight = value;
+  }
+
+  TangibleAsset(
+    this._weight, {
+    required super.name,
+    required super.price,
+    required this.color,
+  });
 }
