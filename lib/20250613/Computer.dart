9111e43561736932d1fb2ca8d8d763152ecd2c7b
sehwan <sehwanq7@gmail.com>
2025-06-13 16:21:57 +0900

    fix: weight에 대한 타입으로 인한 수정

--- lib/20250613/Computer.dart
+++ lib/20250613/Computer.dart
@@ -5,3 +5,8 @@ class Computer extends TangibleAsset {
 
   Computer(super.name, {required super.price, required super.color, required super.weight, required this.makerName});
 }
+
+void main() {
+  Computer computer = Computer('맥프로', price: 2000000, color: 'silver', weight: 2, makerName: 'apple');
+  print('맥프로 무게는 : ${computer.weight}');
+}
--- test/20250613/asset_test.dart
+++ test/20250613/asset_test.dart
@@ -12,13 +12,13 @@ void main() {
 
   group('추상화 킄래스, 인터페이스 테스트', () {
     setUp(() {
-      book = Book('수학', price: 20000, color: 'red', weight: 1, isbn: '1204293409182');
-      computer = Computer('맥프로', price: 2000000, color: 'silver', weight: 2, makerName: 'apple');
+      book = Book('수학', price: 20000, color: 'red', weight: 1.0, isbn: '1204293409182');
+      computer = Computer('맥프로', price: 2000000, color: 'silver', weight: 2.0, makerName: 'apple');
       patent = Patent('식물특허', price: 30000, right: '신품종 보호', country: '한국');
     });
     test('생성 후 결과확인', () {
-      expect(book.weight, 1);
-      expect(computer.weight, 2);
+      expect(book.weight, 1.0);
+      expect(computer.weight, 2.0);
       expect(patent.name, '식물특허');
     });
   });
