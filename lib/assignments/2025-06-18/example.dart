19cb8ace612cd227e82070190ab30c2af16e2ec7
2kihyuk <kihyuk5566@naver.com>
2025-06-18 22:31:03 +0900

    chore : 연습코드 작성

--- lib/assignments/2025-06-18/example.dart
+++ lib/assignments/2025-06-18/example.dart
@@ -107,12 +107,131 @@ class Tank extends MechanicUnit implements Attackable, Terran {
 
   @override
   void attack(Entity entity) {
-    // TODO: implement attack
+    print('Tank가 ${entity.runtimeType}를 공격한다.');
+    entity.hp -= damage;
   }
 
   @override
   void move() {
-    // TODO: implement move
+    print('Tank가 $movingSpeed 속도로 움직인다.');
+  }
+}
+
+class Marine extends BionicUnit implements Terran, Attackable {
+  int _marine_damage;
+
+  Marine(super.movingSpeed, {required super.hp, required int damage})
+    : _marine_damage = damage;
+
+  @override
+  // TODO: implement damage
+  int get damage => _marine_damage;
+
+  @override
+  set damage(int value) {
+    _marine_damage = value;
+  }
+
+  @override
+  void attack(Entity entity) {
+    print('Marine Attack');
+  }
+
+  @override
+  void move() {
+    print('Marine Move');
+  }
+}
+
+class Medic extends BionicUnit implements Terran {
+  Medic(super.movingSpeed, {required super.hp});
+
+  //힐을 하려면 유닛마다 얼마만큼의 힐을 할건지, 힐량이 있어야함.
+
+  void heal(BionicUnit bionicUnit) {
+    bionicUnit.hp += 10;
+  }
+
+  @override
+  void move() {
+    print('Medic Move');
+  }
+}
+
+class Zergling extends BionicUnit implements Zerg, Attackable {
+  int _zergling_damage;
+
+  @override
+  // TODO: implement damage
+  int get damage => _zergling_damage;
+
+  @override
+  set damage(int value) {
+    _zergling_damage = value;
+  }
+
+  Zergling(super.movingSpeed, {required super.hp, required int damage})
+    : _zergling_damage = damage;
+
+  @override
+  void attack(Entity entity) {
+    print('Zergling Attack');
+  }
+
+  @override
+  void move() {
+    print('Zergling Move');
+  }
+
+  @override
+  void regenerate() {
+    print('Zerg 종족의 특성인 자가 회복기능');
+    hp += 10;
+  }
+}
+
+class Zealot extends MechanicUnit implements Protoss, Attackable {
+  int _zealot_damage;
+  int _zealot_shield;
+
+  @override
+  // TODO: implement damage
+  int get damage => _zealot_damage;
+
+  @override
+  set damage(int value) {
+    _zealot_damage = value;
+  }
+
+  @override
+  set shield(int value) {
+    _zealot_shield = value;
+  }
+
+  @override
+  int get shield => _zealot_shield;
+
+  Zealot(
+    super.movingSpeed, {
+    required super.hp,
+    required int damage,
+    required int shield,
+  }) : _zealot_damage = damage,
+       _zealot_shield = shield;
+
+  @override
+  void attack(Entity entity) {
+    print('Zealot Attack');
+  }
+
+  @override
+  void move() {
+    print('Zealot Move');
+  }
+
+  @override
+  void shieldGenerate() {
+    print('Protoss 종족의 특성은 방어막 회복');
   }
 }
 
@@ -133,4 +252,7 @@ void main() {
   for (final item in list) {
     print(item.runtimeType);
   }
+  tank.move();
+  tank.attack(scv1);
+  print(scv1.hp);
 }
