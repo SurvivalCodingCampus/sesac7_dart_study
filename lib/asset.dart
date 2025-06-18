39b6dc792cedc55da72aa3b8e3e1f5352c51c91c
Hagmin.Lee <iilhm2001@naver.com>
2025-06-18 10:05:16 +0900

    feat: Asset 클래스의 price에 대한 유효성 검사 보완

--- lib/asset.dart
+++ lib/asset.dart
@@ -5,7 +5,11 @@ abstract class Asset {
   Asset({required String name, required int price})
     : assert(price >= 0, '자산의 가격은 음수가 될 수 없습니다.'),
       _name = name,
-      _price = price;
+      _price = price {
+    if (price < 0) {
+      throw Exception('자산의 가격은 음수가 될 수 없습니다.');
+    }
+  }
 
   // getter
   String get name => _name;
