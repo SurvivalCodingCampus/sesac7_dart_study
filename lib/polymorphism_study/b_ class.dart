9e603ad693fc86f334a980174cfa6d5f0250a0df
ParkGiTeak <rl980901@naver.com>
2025-06-18 14:06:43 +0900

    feat: 다형성 연습문제 2, 3

---
+++ lib/polymorphism_study/b_ class.dart
@@ -0,0 +1,17 @@
+import 'package:modu_3_dart_study/polymorphism_study/y_abstract_class.dart';
+
+class B extends Y {
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  void c() {
+    print('Bc');
+  }
+}
---
+++ lib/polymorphism_study/y_abstract_class.dart
@@ -0,0 +1,5 @@
+import 'package:modu_3_dart_study/polymorphism_study/x_interface.dart';
+
+abstract class Y implements X {
+  void b();
+}
