245bb6ee7c5bb2f17c4ef28c2d961295e884db66
2kihyuk <kihyuk5566@naver.com>
2025-06-19 20:43:23 +0900

    refactor : 코드 수정

--- lib/assignments/2025-06-19/person.dart
+++ lib/assignments/2025-06-19/person.dart
@@ -1,9 +1,10 @@
 class Address {
-  String street;
+  final String street;
 
   Address({required this.street});
 
-  Address copyWith() => Address(street: street);
+  // Address copyWith() => Address(street: street);
+  Address copyWith({String? street}) => Address(street: street ?? this.street);
 }
 
 class Person {
@@ -13,8 +14,13 @@ class Person {
 
   Person({required this.name, required this.age, required this.address});
 
-  Person copyWith() =>
-      Person(name: name, age: age, address: address.copyWith());
+  // Person copyWith() =>
+  //     Person(name: name, age: age, address: address.copyWith());
+  Person copyWith({String? name, int? age, Address? address}) => Person(
+    name: name ?? this.name,
+    age: age ?? this.age,
+    address: address ?? this.address,
+  );
 }
 
 void main() {
@@ -23,7 +29,8 @@ void main() {
   Person p2 = p1.copyWith();
 
   print(p1.address.hashCode);
-  print(p2.address
-      .hashCode); //깊은 복사를 했기 때문에, address의 hashCode가 다름. 얉은 복사를 통해서 복제하면 같은 address를 가지게되어, 같은 hashCode가 나옴.
+  print(
+    p2.address.hashCode,
+  ); //깊은 복사를 했기 때문에, address의 hashCode가 다름. 얉은 복사를 통해서 복제하면 같은 address를 가지게되어, 같은 hashCode가 나옴.
   print(identical(p1, p2)); // copyWith로 결국 복제하면 다른 객체.
 }
