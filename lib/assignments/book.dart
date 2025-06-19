3c29073c408c5ace19f0cb2272b717191fa53481
0h9u <kog6148@gmail.com>
2025-06-19 16:41:56 +0900

    feat : compareTo 메서드 추가 후 sort 기능 추가

--- lib/assignments/book.dart
+++ lib/assignments/book.dart
@@ -1,4 +1,4 @@
-class Book {
+class Book implements Comparable<Book> {
   String title;
   DateTime publishDate;
   String comment;
@@ -15,4 +15,36 @@ class Book {
 
   @override
   int get hashCode => title.hashCode ^ publishDate.hashCode;
+
+  int compareTo(Book other) {
+    return other.publishDate.compareTo(publishDate);
+  }
+}
+
+void main() {
+  final book1 = Book(
+    title: 'Dart',
+    comment: '언어',
+    publishDate: DateTime(2023, 1, 1),
+  );
+
+  final book2 = Book(
+    title: 'Flutter',
+    comment: '프레임워크',
+    publishDate: DateTime(2024, 1, 1),
+  );
+
+  final book3 = Book(
+    title: 'RiverPod',
+    comment: '라이브러리',
+    publishDate: DateTime(2025, 1, 1),
+  );
+
+  List<Book> books = [book1, book2, book3];
+
+  books.sort();
+
+  for (var book in books) {
+    print('책 제목 : ${book.title}, 날짜 : ${book.publishDate}');
+  }
 }
