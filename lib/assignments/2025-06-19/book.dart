a1dadfd5340201110b83290542c095628b24c84a
Sumin Park <suminp12@gmail.com>
2025-06-19 17:38:14 +0900

    feat: Book class override equals(==), relevant tests are added

--- lib/assignments/2025-06-19/book.dart
+++ lib/assignments/2025-06-19/book.dart
@@ -5,4 +5,17 @@ class Book {
 
   Book({required this.title, required this.comment, DateTime? publishDate})
     : publishDate = publishDate ?? DateTime.now();
+
+  @override
+  bool operator ==(Object other) {
+    // same title && same publishDate => same book!
+    return identical(this, other) ||
+        other is Book &&
+            runtimeType == other.runtimeType &&
+            title == other.title &&
+            publishDate == other.publishDate;
+  }
+
+  @override
+  int get hashCode => title.hashCode ^ publishDate.hashCode;
 }
---
+++ test/assignments/2025-06-19/book_test.dart
@@ -0,0 +1,98 @@
+import 'package:test/test.dart';
+import 'package:modu_3_dart_study/assignments/2025-06-19/book.dart';
+
+void main() {
+  group('GROUP : Book eqaulity override test\n', () {
+    test('Same title, same publishDate', () {
+      //given
+      DateTime date1 = DateTime(1995, 11, 9);
+      DateTime date2 = DateTime(1995, 11, 9);
+
+      Book book1 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date1,
+        comment: "생존코딩 책",
+      );
+      Book book2 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date2,
+        comment: "생존코딩 책",
+      );
+      Book book3 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date1,
+        comment: "생존코딩 책",
+      );
+
+      //when
+
+      //then
+      expect(date1 == date2, true);
+      expect(book1 == book2, true);
+      expect(book1 == book3, true);
+    });
+    test('Same title, different publishDate', () {
+      //given
+      DateTime date1 = DateTime(1995, 11, 9);
+      DateTime date2 = DateTime(1973, 4, 27);
+
+      Book book1 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date1,
+        comment: "생존코딩 책",
+      );
+      Book book2 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date2,
+        comment: "생존코딩 책",
+      );
+      //when
+
+      //then
+      expect(date1 == date2, false);
+      expect(book1 == book2, false);
+    });
+    test('Different title, same publishDate', () {
+      //given
+      DateTime date1 = DateTime(1995, 11, 9);
+      DateTime date2 = DateTime(1995, 11, 9);
+
+      Book book1 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date1,
+        comment: "생존코딩 책",
+      );
+      Book book2 = Book(
+        title: "이준석의 생존코딩",
+        publishDate: date2,
+        comment: "생존코딩 책",
+      );
+      //when
+
+      //then
+      expect(date1 == date2, true);
+      expect(book1 == book2, false);
+    });
+    test('Same title, publishdate, but different comment', () {
+      //given
+      DateTime date1 = DateTime(1995, 11, 9);
+      DateTime date2 = DateTime(1995, 11, 9);
+
+      Book book1 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date1,
+        comment: "생존코딩 책",
+      );
+      Book book2 = Book(
+        title: "오준석의 생존코딩",
+        publishDate: date2,
+        comment: "생존코딩 책일지도 몰라요",
+      );
+      //when
+
+      //then
+      expect(date1 == date2, true);
+      expect(book1 == book2, true);
+    });
+  });
+}
