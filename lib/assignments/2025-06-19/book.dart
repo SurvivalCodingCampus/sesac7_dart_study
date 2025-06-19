a50c39ca6addf43ed2bd8b472dad7ba7855b0fae
Sumin Park <suminp12@gmail.com>
2025-06-19 17:21:51 +0900

    feat: Book class original added

---
+++ lib/assignments/2025-06-19/book.dart
@@ -0,0 +1,8 @@
+class Book {
+  String title;
+  DateTime publishDate;
+  String comment;
+
+  Book({required this.title, required this.comment, DateTime? publishDate})
+    : publishDate = publishDate ?? DateTime.now();
+}
