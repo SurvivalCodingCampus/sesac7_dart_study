1a721272e799ed26e4ded4ab58f71bafa31cb2a5
ParkGiTeak <rl980901@naver.com>
2025-06-20 19:10:33 +0900

    feat: Word Class isConsonant 메서드 추가로 자음 인지 확인하는 함수 개발

--- lib/string_control/word.dart
+++ lib/string_control/word.dart
@@ -12,4 +12,14 @@ class Word {
     String target = word[i];
     return vowel.contains(target);
   }
+
+  bool isConsonant(int i) {
+    if(i >= word.length || i < 0) {
+      throw Exception('범위 지켜라');
+    }
+
+    String vowel = 'aeiou';
+    String target = word[i];
+    return !vowel.contains(target); // 모음에 포함되어 있지 않다면 자음이기 때문에
+  }
 }
