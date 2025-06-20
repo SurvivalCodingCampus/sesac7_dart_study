91bac5823d92502446ebadf4c856c639e4c271c6
0h9u <kog6148@gmail.com>
2025-06-20 20:33:41 +0900

    feat : word 인덱스 자음 체크 코드 추가

--- lib/assignments/word.dart
+++ lib/assignments/word.dart
@@ -8,4 +8,16 @@ class Word {
 
     return _vowel.contains(char);
   }
+
+  bool isConsonant(int i) {
+    if (i < 0 || i >= word.length) return false;
+    String char = word[i].toLowerCase();
+
+    // 정규식 체크
+    final bool isNotWord = !RegExp(r'^[a-z]$').hasMatch(char);
+    if (isNotWord) {
+      return false;
+    }
+    return !_vowel.contains(char);
+  }
 }
