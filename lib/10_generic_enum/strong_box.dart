90c81da8622d8a10902a42758a8878e6cdc3ffdb
SibangGyeon <briankim0695@gmail.com>
2025-06-20 18:10:34 +0900

    feat: 제네릭, 열거형 연습문제 2번 - KeyType 클래스 정의 - StrongBox 클래스 요구사항에 맞게 수정

--- lib/10_generic_enum/strong_box.dart
+++ lib/10_generic_enum/strong_box.dart
@@ -1,11 +1,24 @@
+import 'package:modu_3_dart_study/10_generic_enum/key_type.dart';
+
 class StrongBox<E> {
   E? _instance;
+  final KeyType _keyType;
+  int _count = 0;
+
+  int get count => _count;
+
+  StrongBox({required KeyType keyType}) : _keyType = keyType;
 
   void put(E instance) {
     _instance = instance;
   }
 
   get() {
-    return _instance;
+    if (_keyType.count == _count) {
+      return _instance;
+    } else {
+      _count++;
+      return null;
+    }
   }
 }
