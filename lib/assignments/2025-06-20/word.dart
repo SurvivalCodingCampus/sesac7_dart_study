3a031af281f175acea5da301bad9c836c0673abb
2kihyuk <kihyuk5566@naver.com>
2025-06-21 20:15:59 +0900

    feat : 연습문제 Word 클래스 작성

---
+++ lib/assignments/2025-06-20/word.dart
@@ -0,0 +1,22 @@
+class Word {
+  String word = '';
+
+  bool isVowel(int i) {
+    if (i < 0 || i >= word.length) {
+      throw Exception('$i 번째 글자는 없다.');
+    }
+    String char = word.substring(i, i + 1).toLowerCase();
+    return char == 'a' ||
+        char == 'e' ||
+        char == 'i' ||
+        char == 'o' ||
+        char == 'u';
+  }
+
+  bool isConsonant(int i) {
+    if (i < 0 || i >= word.length) {
+      throw Exception('$i 번째 글자는 없다.');
+    }
+    return !isVowel(i);
+  }
+}
