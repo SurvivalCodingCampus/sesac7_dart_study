ba69d8cfef7d025b547775759aabe512e1a55496
ParkGiTeak <rl980901@naver.com>
2025-06-25 23:31:04 +0900

    test: DefaultFileOperation 파일 복사 테스트 후 파일 정리 코드 추가

--- test/default_file_operantions_test.dart
+++ test/default_file_operantions_test.dart
@@ -14,13 +14,20 @@ void main() {
       testOriginalFile.deleteSync();
     }
 
-    expect(() => DefaultFileOperations().copy(sourcePath: testOriginalFilePath, targetPath: testCopyFilePath), throwsException);
+    expect(
+      () => DefaultFileOperations().copy(
+        sourcePath: testOriginalFilePath,
+        targetPath: testCopyFilePath,
+      ),
+      throwsException,
+    );
   });
 
   test('파일 복사 후 내용 비교', () {
     final String testOriginalFilePath = 'file_test/original_test.txt';
     final String testCopyFilePath = 'file_test/copy_test.txt';
     final File testOriginalFile = File(testOriginalFilePath);
+    final File testCopyFile = File(testCopyFilePath);
     final Directory fileTestDir = Directory('file_test');
 
     if (!fileTestDir.existsSync()) {
@@ -30,11 +37,27 @@ void main() {
     testOriginalFile.createSync();
     testOriginalFile.writeAsStringSync('contents');
 
-    DefaultFileOperations().copy(sourcePath: testOriginalFilePath, targetPath: testCopyFilePath);
+    DefaultFileOperations().copy(
+      sourcePath: testOriginalFilePath,
+      targetPath: testCopyFilePath,
+    );
 
-    final String originalFileContent = File(testOriginalFilePath).readAsStringSync();
+    final String originalFileContent = File(
+      testOriginalFilePath,
+    ).readAsStringSync();
     final String copyFileContent = File(testCopyFilePath).readAsStringSync();
 
     expect(originalFileContent, equals(copyFileContent));
+
+    // 테스트 후 정리
+    if (testOriginalFile.existsSync()) {
+      testOriginalFile.deleteSync();
+    }
+    if (testCopyFile.existsSync()) {
+      testCopyFile.deleteSync();
+    }
+    if (fileTestDir.existsSync()) {
+      fileTestDir.deleteSync();
+    }
   });
 }
