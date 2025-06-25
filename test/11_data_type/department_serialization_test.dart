b40f33b3023049f91f1df0a5ebe51c19f8d60a61
SibangGyeon <briankim0695@gmail.com>
2025-06-25 18:30:27 +0900

    test: Department 클래스 직렬화 테스트 추가

---
+++ test/11_data_type/department_serialization_test.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:modu_3_dart_study/11_data_type/department.dart';
+import 'package:modu_3_dart_study/11_data_type/employee.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('Department 직렬화 / 역직렬화 테스트', () {
+    final String name = '홍길동';
+    final String departmentName = '총무부';
+    final int age = 41;
+    final employee = Employee(name: name, age: age);
+    final department = Department(name: departmentName, leader: employee);
+    final String departmentStringJson =
+        '''{"name":"총무부","leader":{"name":"홍길동","age":41}}''';
+
+    test('Department 직렬화 테스트', () {
+      try {
+        Map<String, dynamic> departmentJson = department.toJson();
+        final file = File('company.txt');
+        final String testDepartmentStringJson = jsonEncode(departmentJson);
+
+        file.writeAsStringSync(testDepartmentStringJson);
+
+        final testStringJson = file.readAsStringSync();
+        final testJson = jsonEncode(departmentJson);
+
+        expect(testJson == departmentStringJson, equals(true));
+        expect(testStringJson == departmentStringJson, equals(true));
+      } catch (e) {
+        throw Exception();
+      }
+    });
+
+    test('Department 역직렬화 테스트', () {
+      try {
+        final file = File('company.txt');
+        final testStringJson = file.readAsStringSync();
+        final testDepartment = Department.fromJson(jsonDecode(testStringJson));
+
+        expect(testStringJson == departmentStringJson, equals(true));
+
+        expect(testDepartment.name == departmentName, equals(true));
+        expect(testDepartment.leader.name == name, equals(true));
+        expect(testDepartment.leader.age == age, equals(true));
+      } catch (e) {
+        throw Exception();
+      }
+    });
+  });
+}
