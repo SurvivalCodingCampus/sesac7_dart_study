56041449f9b6ea45d0322ee76c0791c2bf4b0e40
sehwan <sehwanq7@gmail.com>
2025-06-25 22:03:35 +0900

    fix: 오타수정

--- lib/20250625/my_exception.dart
+++ lib/20250625/my_exception.dart
@@ -1,7 +1,7 @@
 class MyException implements Exception {
   static const FILE_NOT_FOUND = '파일을 찾을 수 없습니다.';
   static const FILE_CONTENTS_EMPTY = '파일 내용이 없습니다.';
-  static const FILE_NAME_EMPTY = '경로가 비어있습니다';
+  static const FILE_NAME_EMPTY = '경로가 비어있습니다.';
 
   final String? _message;
 
