140bb762feee4a0a41d206c4d3f7777a4d898a04
Sumin Park <suminp12@gmail.com>
2025-06-25 16:30:05 +0900

    feat: StrongBox now has new field keyType and _tryCount, relevant tests are added

--- lib/assignments/2025-06-25/strong_box.dart
+++ lib/assignments/2025-06-25/strong_box.dart
@@ -1,13 +1,36 @@
-class StrongBox<T> {
-  T? _content;
+import 'package:modu_3_dart_study/assignments/2025-06-25/key_type.dart';
 
-  StrongBox({T? content}) : _content = content;
+class StrongBox<T> {
+  static const Map<KeyType, int> _maxTrials = {
+    KeyType.padlock: 1024,
+    KeyType.button: 10000,
+    KeyType.dial: 30000,
+    KeyType.finger: 1_000_000,
+  };
+
+  T? _content;
+  final KeyType keyType;
+  int _tryCount = 0;
+
+  StrongBox(this.keyType, {T? content}) : _content = content;
 
   T? get() {
+    if (_tryCount < StrongBox._maxTrials[keyType]!) {
+      //print("$_tryCount out of ${StrongBox._maxTrials[keyType]!}");
+      _tryCount++;
+      return null;
+    }
     return _content;
   }
 
   void set(T item) {
-    _content = item;
+    if (_content == null) {
+      _content = item;
+      _tryCount = 0;
+    } else {
+      print(
+        "The strongbox is already occupied. Need to open it before putting new items.",
+      );
+    }
   }
 }
---
+++ test/assignments/2025-06-25/strong_box_test.dart
@@ -0,0 +1,64 @@
+import 'package:modu_3_dart_study/assignments/2025-06-25/key_type.dart';
+import 'package:test/test.dart';
+import 'package:modu_3_dart_study/assignments/2025-06-25/strong_box.dart';
+
+void main() {
+  group('GROUP : StrongBox tests\n', () {
+    test('Set and get try: padlock', () {
+      //given
+      StrongBox<String> box = StrongBox(KeyType.padlock);
+      expect(box.get(), null);
+      //when
+      box.set("Secret Map");
+      box.set("Different Map");
+      //then
+      for (int i = 0; i < 1024; i++) {
+        // 1024 is the StrongBox._maxTrials[padlock]
+        expect(box.get(), null);
+      }
+      expect(box.get(), "Secret Map");
+    });
+    test('Set and get try: button', () {
+      //given
+      StrongBox<String> box = StrongBox(KeyType.button);
+      expect(box.get(), null);
+      //when
+      box.set("Secret Map");
+      box.set("Different Map");
+      //then
+      for (int i = 0; i < 10000; i++) {
+        // 10000 is the StrongBox._maxTrials[button]
+        expect(box.get(), null);
+      }
+      expect(box.get(), "Secret Map");
+    });
+    test('Set and get try: dial', () {
+      //given
+      StrongBox<String> box = StrongBox(KeyType.dial);
+      expect(box.get(), null);
+      //when
+      box.set("Secret Map");
+      box.set("Different Map");
+      //then
+      for (int i = 0; i < 30000; i++) {
+        // 30000 is the StrongBox._maxTrials[dial]
+        expect(box.get(), null);
+      }
+      expect(box.get(), "Secret Map");
+    });
+    test('Set and get try: finger', () {
+      //given
+      StrongBox<String> box = StrongBox(KeyType.finger);
+      expect(box.get(), null);
+      //when
+      box.set("Secret Map");
+      box.set("Different Map");
+      //then
+      for (int i = 0; i < 1_000_000; i++) {
+        // 1_000_000 is the StrongBox._maxTrials[finger]
+        expect(box.get(), null);
+      }
+      expect(box.get(), "Secret Map");
+    });
+  });
+}
