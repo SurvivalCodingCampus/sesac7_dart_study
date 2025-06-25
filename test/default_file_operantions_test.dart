b3c4073bae4c9b42c001a33b50bbface6285776c
ParkGiTeak <rl980901@naver.com>
2025-06-25 18:48:26 +0900

    test: DefaultFileOperations Class copy 메서드 테스트 코드 작성

---
+++ test/default_file_operantions_test.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:modu_3_dart_study/file_control/default_file_operations.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  test('원본 파일 경로가 없는 경우 Throw 테스트', () {
+    final String testOriginalFilePath = 'file_test/original_test.txt';
+    final String testCopyFilePath = 'file_test/copy_test.txt';
+    final File testOriginalFile = File(testOriginalFilePath);
+
+    if (testOriginalFile.existsSync()) {
+      testOriginalFile.deleteSync();
+    }
+
+    expect(() => DefaultFileOperations().copy(sourcePath: testOriginalFilePath, targetPath: testCopyFilePath), throwsException);
+  });
+
+  test('파일 복사 후 내용 비교', () {
+    final String testOriginalFilePath = 'file_test/original_test.txt';
+    final String testCopyFilePath = 'file_test/copy_test.txt';
+    final File testOriginalFile = File(testOriginalFilePath);
+
+    testOriginalFile.writeAsStringSync('contents');
+
+    DefaultFileOperations().copy(sourcePath: testOriginalFilePath, targetPath: testCopyFilePath);
+
+    final String originalFileContent = File(testOriginalFilePath).readAsStringSync();
+    final String copyFileContent = File(testCopyFilePath).readAsStringSync();
+
+    expect(originalFileContent, equals(copyFileContent));
+  });
+}
