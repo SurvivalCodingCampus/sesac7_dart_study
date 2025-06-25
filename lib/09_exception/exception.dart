f0b260dc4c1484d284ae1dbddec833056782ab82
ParkGiTeak <rl980901@naver.com>
2025-06-25 23:34:16 +0900

    feat: exception1 함수 구체적인 예외처리 추가

--- lib/09_exception/exception.dart
+++ lib/09_exception/exception.dart
@@ -4,7 +4,11 @@ void exception1() {
   int num;
   try {
     num = int.parse(numString);
+  } on FormatException {
+    print('숫자 형식이 올바르지 않습니다: $numString');
+    num = 0;
   } catch (e) {
+    print('예상치 못한 오류가 발생했습니다: $e');
     num = 0;
   }
 
