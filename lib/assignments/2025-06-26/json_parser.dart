bd7e14b6ea9ca94807ecf442b4a6ed0075f447f1
Sumin Park <suminp12@gmail.com>
2025-06-26 18:05:49 +0900

    feat: json_parser class added to parse certain json file format into a data collection

---
+++ lib/assignments/2025-06-26/json_parser.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'dart:io';
+
+class PriceData {
+  double price;
+  DateTime cvtDatetime;
+
+  PriceData(this.price, this.cvtDatetime);
+
+  PriceData.fromJson(Map<String, dynamic> json)
+    : price = json['price'] ?? 0.0,
+      cvtDatetime =
+          DateTime.tryParse(json['cvtDatetime'] ?? "0000-01-01") ??
+          DateTime(0, 1, 1);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'price': price,
+      'cvtDatetime':
+          '${cvtDatetime.year}-${cvtDatetime.month}-${cvtDatetime.day}T${cvtDatetime.hour}:${cvtDatetime.minute}:${cvtDatetime.second}',
+    };
+  }
+}
+
+class PriceCollection {
+  String collectionName;
+  List<PriceData>? collectionSalePrice;
+
+  PriceCollection(this.collectionName);
+
+  PriceCollection.fromJson(Map<String, dynamic> json)
+    : collectionName = json['collectionName'] ?? '',
+      collectionSalePrice = ((json['collectionSalePrice'] ?? []) as List)
+          .map((e) => PriceData.fromJson(e))
+          .toList();
+
+  Map<String, dynamic> toJson() {
+    if (collectionSalePrice == null) {
+      return {'collectionName': collectionName, 'collectionSalePrice': '[]'};
+    }
+    return {
+      'collectionName': collectionName,
+      'collectionSalePrice': collectionSalePrice!
+          .map((e) => e.toJson())
+          .toList(),
+    };
+  }
+}
+
+class CollectionList {
+  List<PriceCollection>? collectionChartDataList;
+
+  CollectionList();
+
+  CollectionList.fromJson(Map<String, dynamic> json)
+    : collectionChartDataList =
+          ((json['collectionChartDataList'] ?? []) as List)
+              .map((e) => PriceCollection.fromJson(e))
+              .toList();
+
+  Map<String, dynamic> toJson() {
+    if (collectionChartDataList == null) {
+      return {};
+    }
+    return {
+      'collectionChartDataList': collectionChartDataList!
+          .map((e) => e.toJson())
+          .toList(),
+    };
+  }
+}
+
+void main() {
+  final file = File('lib/assignments/2025-06-26/chart_data.json');
+  final fileString = file.readAsStringSync();
+
+  final jsonMap = jsonDecode(fileString);
+  final dataChunk = CollectionList.fromJson(jsonMap);
+
+  print(dataChunk);
+}
