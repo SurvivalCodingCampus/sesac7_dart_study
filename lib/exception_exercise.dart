74a7a44cfe1d50cc3f027e08e65e31f60b759404
wtoshm <wtoshm@gmail.com>
2025-06-26 11:09:34 +0900

    refactor: exception_exercise 수정

--- lib/exception_exercise.dart
+++ lib/exception_exercise.dart
@@ -1,11 +1,11 @@
 void main() {
   final doubleString = '10.5';
+  int num = 0;
 
   try {
-    int num = int.parse(doubleString);
-    print(num);
+    num = int.parse(doubleString);
   } catch (e) {
-    int num = 0;
+    num = 0;
     print('예외처리: int.parse 함수의 인자로 interger 타입이 아닌 인자가 들어와서 $num 으로 처리하였음.');
   }
 }
