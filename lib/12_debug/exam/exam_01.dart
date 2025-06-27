1f88d6565f4e11e5ba821327cfdee74c7a4c7f08
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-06-27 14:19:19 +0900

    feat: `query.dart` 및 `exam_01.dart` 연습문제 풀이
    - `query.dart`: 트랜잭션 데이터 기반 8가지 질의응답 기능 추가
    - `exam_01.dart`: CollectionSalePrice, CollectionChartData 클래스 및 직렬화/역직렬화, copyWith 기능 추가

---
+++ lib/12_debug/exam/exam_01.dart
@@ -0,0 +1,76 @@
+class CollectionSalePrice {
+  final double _price;
+  final String _cvtDatetime;
+
+  double get price => _price;
+  String get cvtDatetime => _cvtDatetime;
+
+  CollectionSalePrice(double price, String cvtDatetime): _price = price, _cvtDatetime = cvtDatetime;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is CollectionSalePrice &&
+              runtimeType == other.runtimeType &&
+              _price == other._price &&
+              _cvtDatetime == other._cvtDatetime;
+
+  @override
+  int get hashCode => _price.hashCode ^ _cvtDatetime.hashCode;
+
+  CollectionSalePrice copyWith({
+    double? price,
+    String? cvtDatetime,
+  }) {
+    return CollectionSalePrice(
+      price ?? _price,
+      cvtDatetime ?? _cvtDatetime,
+    );
+  }
+
+  CollectionSalePrice.fromJson(Map<String, dynamic> json) :
+        _price = (json['price'] as num).toDouble(),
+        _cvtDatetime = json['cvtDatetime'] as String;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'price' : _price,
+      'cvtDatetime' : _cvtDatetime
+    };
+  }
+}
+
+class CollectionChartData {
+  final List<CollectionSalePrice>? _collectionSalePrice;
+  final String _collectionName;
+
+  CollectionChartData(List<CollectionSalePrice>? collectionSalePrice, String collectionName)
+      :_collectionSalePrice = collectionSalePrice, _collectionName = collectionName;
+
+  String get collectionName => _collectionName;
+  List<CollectionSalePrice>? get collectionSalePrice => _collectionSalePrice;
+
+
+
+  CollectionChartData.fromJson(Map<String, dynamic> json) :
+        _collectionName = json['collectionName'],
+        _collectionSalePrice = json['collectionSalePrice'] != null ?
+        (json['collectionSalePrice'] as List<dynamic>).map((e) => CollectionSalePrice.fromJson(e)).toList() : null;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'collectionSalePrice' : _collectionSalePrice?.map((e) => e.toJson()).toList(),
+      'collectionName' : _collectionName
+    };
+  }
+
+  CollectionChartData copyWith({
+    List<CollectionSalePrice>? collectionSalePrice,
+    String? collectionName,
+  }) {
+    return CollectionChartData(
+      collectionSalePrice ?? this._collectionSalePrice,
+      collectionName ?? this._collectionName,
+    );
+  }
+}
--- lib/13_fp/exam/query.dart
+++ lib/13_fp/exam/query.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 
 class Trader {
@@ -25,6 +27,37 @@ final transactions = [
 ];
 
 void main() {
+  // 8. 전체 트랜잭션 중 최소값은 얼마인가?
+  print(transactions.map((e) => e.value).min);
+
+  // 7. 전체 트랜잭션 중 최대값은 얼마인가?
+  print(transactions.map((e) => e.value).fold<int>(0, max));
+
+  // 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 출력하시오
+  print(
+    transactions.where((e) => e.trader.city == 'Cambridge').map((e) => e.value),
+  );
+
+  // 5. 밀라노에 거래자가 있는가?
+  print(transactions.any((e) => e.trader.city == 'Milan'));
+
+  // 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
+  print(transactions.map((e) => e.trader.name).toSet().sorted());
+
+  // 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
+  print(
+    transactions
+        .where((e) => e.trader.city == 'Cambridge')
+        .sortedBy((e) => e.trader.name)
+        .map((e) => e.trader.name)
+        .toSet(),
+  );
+
+  // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
+  print(
+    transactions.map((e) => e.trader.city).toSet(),
+  );
+
   // 1. 2011년에 일어난 모든 트랜잭션을 찾아 가격 기준 오름차순으로 정리하여 이름을 나열하시오
   print(
     transactions
