9b27a35c9a534b8984c5daf8343d324cbb6e72d5
Sumin Park <suminp12@gmail.com>
2025-06-27 16:29:01 +0900

    chore: unifinished movies.dart added

---
+++ lib/assignments/2025-06-27/movies.dart
@@ -0,0 +1,14 @@
+Future<Movie> getMovieInfo() {
+  // TODO : 2초간 기다리는 코드 작성
+
+  // 서버에서 들어오는 데이터라고 상상
+
+  final String jsonString = '''{
+    "title": "Star Ward",
+    "director": "George Lucas",
+    "year": 1977
+  }''';
+
+  // TODO : Movie 데이터 클래스를 리턴하도록 작성
+  return
+}
