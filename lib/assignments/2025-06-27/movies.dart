edf8a288ea0c0a51cbb7e70346bac922068a9d65
Sumin Park <suminp12@gmail.com>
2025-06-27 16:45:31 +0900

    feat: Movie class introduced to support movie data with basic class methods

--- lib/assignments/2025-06-27/movies.dart
+++ lib/assignments/2025-06-27/movies.dart
@@ -1,3 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Movie {
+  String? title;
+  String? director;
+  int? year;
+
+  Movie ({String? title, String? director, int? year});
+
+  Movie.fromJson(Map<String, dynamic> json) : title = json['title'], director = json['director'], year = json['year'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title' : title,
+      'director' : director,
+      'year' : year
+    };
+  }
+  
+
+  Movie copyWith({
+    String? title,
+    String? director,
+    int? year,
+  }) {
+    return Movie(
+      title: title ?? this.title,
+      director: director ?? this.director,
+      year: year ?? this.year,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+    other is Movie && runtimeType == other.runtimeType &&
+    title == other.title && director == other.director && year == other.year;
+  }
+
+  @override
+  int get hashCode => title.hashCode ^ director.hashCode ^ year.hashCode;
+
+  @override
+  String toString() => 'Movie(title: $title, director: $director, year: $year)';
+}
+
 Future<Movie> getMovieInfo() {
   // TODO : 2초간 기다리는 코드 작성
 
