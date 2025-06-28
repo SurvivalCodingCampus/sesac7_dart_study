6fe43f1fe8de175ac9f489aceb2900dbef8a21b8
jason lim <7newer@gmail.com>
2025-06-28 21:55:54 +0900

    feat: 비동기 연습문제2

--- test/testcode/asynchronous_test.dart
+++ test/testcode/asynchronous_test.dart
@@ -2,7 +2,7 @@ import 'package:modu_3_dart_study/lecture_code/asynchronous/asynchronous.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('description', () async {
+  test('비동기 연습문제 1번 반환한 json 확인', () async {
     //given(준비)
     final String title = "StarWard";
     final String director = "George Lucas";
@@ -15,6 +15,7 @@ void main() {
 
     //then(검증)
     expect(end.difference(start) >= Duration(seconds: 2), true);
+    //external Duration difference(DateTime other);
     expect(movie.title == title, true);
     expect(movie.director == director, true);
     expect(movie.year == year, true);
