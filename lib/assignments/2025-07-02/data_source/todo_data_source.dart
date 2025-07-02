f99ac9511a7e4c3662fbb967e7df7d02bbfe1f0b
wtoshm <wtoshm@gmail.com>
2025-07-02 14:34:09 +0900

    feat : TodoDataSource 에getTodos() 추가

--- lib/assignments/2025-07-02/data_source/todo_data_source.dart
+++ lib/assignments/2025-07-02/data_source/todo_data_source.dart
@@ -2,4 +2,6 @@ import 'package:modu_3_dart_study/assignments/2025-07-02/models/todo.dart';
 
 abstract interface class TodoDataSource {
   Future<Todo> getTodo();
+
+  Future<List<Todo>> getTodos();
 }
