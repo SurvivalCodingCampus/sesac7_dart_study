bfa215e30ad43641aba437336069956d2ed077ee
wtoshm <wtoshm@gmail.com>
2025-07-02 20:25:58 +0900

    feat : user.dart 추가

---
+++ lib/assignments/2025-07-02/models/user.dart
@@ -0,0 +1,82 @@
+class User {
+  int id;
+  String name;
+  String username;
+  String email;
+  Address address;
+  String phone;
+  String website;
+  Company company;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phone,
+    required this.website,
+    required this.company,
+  });
+
+  User.fromJson(Map<String, dynamic> json)
+    : id = json['id'],
+      name = json['name'],
+      username = json['username'],
+      email = json['email'],
+      address = json['address'],
+      phone = json['phone'],
+      website = json['website'],
+      company = json['company'];
+}
+
+class Address {
+  String street;
+  String suite;
+  String city;
+  String zipcode;
+  Geo geo;
+
+  Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+    required this.geo,
+  });
+
+  Address.fromJson(Map<String, String> json)
+    : street = json['street'] ?? '',
+      suite = json['suite'] ?? '',
+      city = json['city'] ?? '',
+      zipcode = json['zipcode'] ?? '',
+      geo = (json['geo'] as Geo?) ?? Geo(lat: '', lng: '');
+}
+
+class Geo {
+  String lat;
+  String lng;
+
+  Geo({required this.lat, required this.lng});
+
+  Geo.fromJson(Map<String, String> json)
+    : lat = json['lat'] ?? '',
+      lng = json['lng'] ?? '';
+}
+
+class Company {
+  String name;
+  String catchPhrase;
+  String bs;
+
+  Company({
+    required this.name,
+    required this.catchPhrase,
+    required this.bs,
+  });
+
+  Company.fromJson(Map<String, String> json)
+    : name = json['name'] ?? '',
+      catchPhrase = json['catchPhrase'] ?? '',
+      bs = json['bs'] ?? '';
+}
