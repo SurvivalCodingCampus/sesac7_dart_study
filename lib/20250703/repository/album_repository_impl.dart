11ffa7d1d167f496be31e5111fdd57569021ccf5
sehwan <sehwanq7@gmail.com>
2025-07-03 19:27:04 +0900

    feat: Album 데이터 처리 및 테스트 코드 추가
    - Album 모델, Repository, DataSource 클래스 및 인터페이스 추가
    - `json_serializable` 및 `mockito` 라이브러리 추가
    - Album 데이터 가져오기 및 필터링 기능 구현
    - Album Repository 테스트 코드 작성 및 `album_test.mocks.dart` 생성
    - `.gitignore`에 `*.mocks.dart` 추가

---
+++ lib/20250703/repository/album_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:collection/collection.dart';
+import 'package:modu_3_dart_study/20250703/data_source/album_data_source.dart';
+import 'package:modu_3_dart_study/20250703/model/album.dart';
+import 'package:modu_3_dart_study/20250703/repository/album_repository.dart';
+
+class AlbumRepositoryImpl implements AlbumRepository{
+  final AlbumDataSource _albumDataSource;
+  
+  AlbumRepositoryImpl(this._albumDataSource);
+
+  @override
+  Future<List<Album>> getAlbums({int? limit}) async {
+    try {
+      final result = await _albumDataSource.getAllData();
+      if(limit == null || limit <= 0) {
+        return result
+            .map((e) => Album.fromJson(e)).toList();
+      } else {
+        return result
+            .map((e) => Album.fromJson(e))
+            .toList().sublist(0, limit);
+      }
+    } catch (e) {
+      return [];
+    }
+  }
+}
