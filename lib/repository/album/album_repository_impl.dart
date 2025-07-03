d35e863999d7ea5ab9fa59be101a4fda34e5b254
ParkGiTeak <rl980901@naver.com>
2025-07-03 21:23:49 +0900

    feat: AlbumRepository interface, AlbumRepositoryImpl 구현체 Class 구현 및 추가

---
+++ lib/repository/album/album_repository_impl.dart
@@ -0,0 +1,33 @@
+import 'package:collection/collection.dart';
+import 'package:modu_3_dart_study/data_source/album/album_data_source.dart';
+import 'package:modu_3_dart_study/data_source/album/album_data_source_impl.dart';
+import 'package:modu_3_dart_study/model/album.dart';
+import 'package:modu_3_dart_study/repository/album/album_repository.dart';
+
+class AlbumRepositoryImpl implements AlbumRepository {
+  final AlbumDataSource _albumDataSourceImpl;
+
+  AlbumRepositoryImpl({required AlbumDataSource albumDataSourceImpl})
+    : _albumDataSourceImpl = albumDataSourceImpl;
+
+  @override
+  Future<List<Album>> getAlbums({int? limit}) async {
+    final List<Map<String, dynamic>> albumJsonList = await _albumDataSourceImpl
+        .getAllAlbums();
+    final List<Album> albums = albumJsonList
+        .map((album) => Album.fromJson(album))
+        .toList();
+
+    if (limit != null) {
+      return albums.slice(0, limit);
+    } else {
+      return albums;
+    }
+  }
+}
+
+void main() async {
+  final albumDataSource = AlbumDataSourceImpl(filePath: 'json_data/albums.json');
+  final albumRepository = AlbumRepositoryImpl(albumDataSourceImpl: albumDataSource);
+  print(await albumRepository.getAlbums(limit: 4));
+}
