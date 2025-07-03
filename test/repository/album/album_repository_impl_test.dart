bac149c767645b3451d3219ddcd24b41af17a675
ParkGiTeak <rl980901@naver.com>
2025-07-03 21:38:45 +0900

    test: AlbumRepositoryImpl 테스트 코드 추가

---
+++ test/repository/album/album_repository_impl_test.dart
@@ -0,0 +1,39 @@
+import 'package:modu_3_dart_study/data_source/album/album_data_source.dart';
+import 'package:modu_3_dart_study/model/album.dart';
+import 'package:modu_3_dart_study/repository/album/album_repository.dart';
+import 'package:modu_3_dart_study/repository/album/album_repository_impl.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+import '../../mocks/mock_album_data_source_impl.dart';
+
+void main() {
+  group('AlbumRepositoryImpl Test', () {
+    test('getAlbums test', () async {
+      final AlbumDataSource mockAlbumDataSourceImpl = MockAlbumDataSourceImpl();
+      final AlbumRepository albumRepository = AlbumRepositoryImpl(albumDataSourceImpl: mockAlbumDataSourceImpl);
+      final List<Album> albums = await albumRepository.getAlbums();
+
+      expect(albums.length, equals(10));
+      expect(albums.first.userId, equals(1));
+      expect(albums.first.id, equals(1));
+      expect(albums.first.title, equals('quidem molestiae enim'));
+    });
+    test('getAlbums parameter limit test', () async {
+      final AlbumDataSource mockAlbumDataSourceImpl = MockAlbumDataSourceImpl();
+      final AlbumRepository albumRepository = AlbumRepositoryImpl(albumDataSourceImpl: mockAlbumDataSourceImpl);
+      final int totalAlbumSize = (await albumRepository.getAlbums()).length;
+
+      for (int i = 0; i < totalAlbumSize; i++) {
+        expect((await albumRepository.getAlbums(limit: i)).length, equals(i));
+      }
+    });
+    test('getAlbums parameter limit 전체 앨범 범위 벗어나면 exception 발생 테스트', () async {
+      final AlbumDataSource mockAlbumDataSourceImpl = MockAlbumDataSourceImpl();
+      final AlbumRepository albumRepository = AlbumRepositoryImpl(albumDataSourceImpl: mockAlbumDataSourceImpl);
+      final int totalAlbumSize = (await albumRepository.getAlbums()).length;
+
+      expect(() async => await albumRepository.getAlbums(limit: totalAlbumSize + 1), throwsException);
+    });
+  });
+}
