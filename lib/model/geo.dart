7325f41c2d195ff762b640c9383859e6b01b6868
ParkGiTeak <rl980901@naver.com>
2025-07-03 20:35:41 +0900

    fix: Geo Model Class lon 필드 이름 lng로 수정

--- lib/model/geo.dart
+++ lib/model/geo.dart
@@ -5,24 +5,24 @@ part 'geo.g.dart';
 @JsonSerializable(explicitToJson: true)
 class Geo {
   final String? lat;
-  final String? lon;
+  final String? lng;
 
-  Geo({this.lat, this.lon});
+  Geo({this.lat, this.lng});
 
   factory Geo.fromJson(Map<String, dynamic> json) => _$GeoFromJson(json);
 
   @override
-  String toString() => 'Geo(lat: $lat, lon: $lon)';
+  String toString() => 'Geo(lat: $lat, lng: $lng)';
 
   @override
-  int get hashCode => Object.hash(lat, lon);
+  int get hashCode => Object.hash(lat, lng);
 
   @override
   bool operator ==(Object other) =>
-      other is Geo ? lat == other.lat && lon == other.lon : false;
+      other is Geo ? lat == other.lat && lng == other.lng : false;
 
   Map<String, dynamic> toJson() => _$GeoToJson(this);
 
   Geo copyWith({String? lat, String? lon}) =>
-      Geo(lat: lat ?? this.lat, lon: lon ?? this.lon);
+      Geo(lat: lat ?? this.lat, lng: lon ?? this.lng);
 }
