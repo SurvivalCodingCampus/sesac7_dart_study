cbdb65b9c39c7ddaaa2ad38657f5d5f91fc1bcf4
ParkGiTeak <rl980901@naver.com>
2025-07-03 18:22:51 +0900

    feat: Comment Model Class 추가

---
+++ lib/model/comment.dart
@@ -0,0 +1,41 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'comment.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Comment {
+  final int postId;
+  final int id;
+  final String name;
+  final String email;
+  final String body;
+
+  const Comment({
+    required this.postId,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.body,
+  });
+
+  factory Comment.fromJson(Map<String, dynamic> json) =>
+      _$CommentFromJson(json);
+
+  @override
+  bool operator ==(Object other) => other is Comment
+      ? postId == other.postId &&
+            id == other.id &&
+            name == other.name &&
+            email == other.email &&
+            body == other.body
+      : false;
+
+  @override
+  int get hashCode => Object.hash(postId, id, name, email, body);
+
+  @override
+  String toString() =>
+      'Comment(postId: $postId, id: $id, name: $name, email: $email, body: $body)';
+
+  Map<String, dynamic> toJson() => _$CommentToJson(this);
+}
