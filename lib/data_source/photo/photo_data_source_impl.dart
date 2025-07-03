b578b088138b018d45672bf391332a1699ac03c2
ParkGiTeak <rl980901@naver.com>
2025-07-03 18:39:45 +0900

    feat: PhotoDataSource Interface, PhotoDataSourceImpl 구현체 Class 구현 및 추가

---
+++ lib/data_source/photo/photo_data_source_impl.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:modu_3_dart_study/data_source/photo/photo_data_source.dart';
+
+class PhotoDataSourceImpl implements PhotoDataSource {
+  final String filePath;
+
+  PhotoDataSourceImpl({required this.filePath});
+
+  @override
+  Future<List<Map<String, dynamic>>> getJsonList() async {
+    final File jsonFile = File(filePath);
+
+    if (await jsonFile.exists()) {
+      final String jsonString = await jsonFile.readAsString();
+      return (jsonDecode(jsonString) as List)
+          .map((photo) => photo as Map<String, dynamic>)
+          .toList();
+    } else {
+      return [{}];
+    }
+  }
+}
