c2de488dfadc51ba238d65286fae74f81b868b29
Jaesung lim <7newer@gmail.com>
2025-07-04 21:55:23 +0900

    fix: 네트워크 통신 연습문제 2번 수정중

--- lib/18_http/data_source/remote_data_source_impl.dart
+++ lib/18_http/data_source/remote_data_source_impl.dart
@@ -6,13 +6,17 @@ import 'package:modu_3_dart_study/18_http/model/post.dart';
 import 'package:http/http.dart' as http;
 
 class RemoteDataSourceImpl implements RemoteDataSource {
-  static const _baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  http.Client _client;
+
+
 
   // post
   @override
   Future<ResponseCore<Post>> createPost(Post post) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl/posts'), // Uri.parse()는 문자열 주소를 http 패키지가 사용하기 좋은 Uri 객체 형태로 변환
+      Uri.parse('$baseUrl/posts'), // Uri.parse()는 문자열 주소를 http 패키지가 사용하기 좋은 Uri 객체 형태로 변환
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(post),
     );
@@ -26,7 +30,7 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   // delete
   @override
   Future<ResponseCore<void>> deletePost(int id) async {
-    final response = await http.delete(Uri.parse('$_baseUrl/posts/$id'),);
+    final response = await http.delete(Uri.parse('$baseUrl/posts/$id'),);
     if (response.statusCode == 204) {
       return ResponseCore(statusCode: response.statusCode,
           header: response.headers,
@@ -40,7 +44,7 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   // get with id
   @override
   Future<ResponseCore<Post>> getPost(int id) async {
-    final response = await http.get(Uri.parse('$_baseUrl/posts/$id'),);
+    final response = await http.get(Uri.parse('$baseUrl/posts/$id'),);
     return ResponseCore(statusCode: response.statusCode,
       header: response.headers,
       body: jsonDecode(response.body),);
@@ -49,7 +53,7 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   // get
   @override
   Future<ResponseCore<List<Post>>> getPosts() async {
-    final response = await http.get(Uri.parse('$_baseUrl/posts'),);
+    final response = await http.get(Uri.parse('$baseUrl/posts'),);
     return ResponseCore(statusCode: response.statusCode,
       header: response.headers,
       body: jsonDecode(response.body),);
@@ -59,7 +63,7 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   @override
   Future<ResponseCore<Post>> patchPost(int id,
       Map<String, dynamic> postData) async {
-    final response = await http.patch(Uri.parse('$_baseUrl/posts/$id'),);
+    final response = await http.patch(Uri.parse('$baseUrl/posts/$id'),);
     return ResponseCore(statusCode: response.statusCode,
       header: response.headers,
       body: jsonDecode(response.body),);
@@ -68,7 +72,7 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   // update
   @override
   Future<ResponseCore<Post>> updatePost(int id, Post post) async {
-    final response = await http.put(Uri.parse('$_baseUrl/posts/$id'),);
+    final response = await http.put(Uri.parse('$baseUrl/posts/$id'),);
     return ResponseCore(statusCode: response.statusCode,
       header: response.headers,
       body: jsonDecode(response.body),);
