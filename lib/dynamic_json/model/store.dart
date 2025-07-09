315aec4edc66e31620f332932e5cf569745bf9b4
Hagmin.Lee <iilhm2001@naver.com>
2025-07-10 01:35:20 +0900

    feat: Store 클래스 생성(Map 내 존재하는 모델 클래스 분기 생성)

--- lib/dynamic_json/model/store.dart
+++ lib/dynamic_json/model/store.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:modu_3_dart_study/dynamic_json/dto/store_result_dto.dart';
 
 part 'store.freezed.dart';
 part 'store.g.dart';
@@ -7,8 +6,15 @@ part 'store.g.dart';
 @freezed
 abstract class Store with _$Store {
   const factory Store({
-    required int count,
-    required List<StoreDto> stores,
+    required addr,
+    required code,
+    required createdAt,
+    required lat,
+    required lng,
+    required name,
+    required remainStat,
+    required stockAt,
+    required type,
   }) = _Store;
 
   factory Store.fromJson(Map<String, Object?> json) => _$StoreFromJson(json);
