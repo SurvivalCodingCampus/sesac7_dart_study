62cc6633993ae9769351ad655f8b322dd0d36ccb
Hagmin.Lee <iilhm2001@naver.com>
2025-07-09 20:27:51 +0900

    feat: Photo 클래스 관련 Mapper 생성

---
+++ lib/dto_mapper/mapper/photo_mapper.dart
@@ -0,0 +1,41 @@
+import 'package:modu_3_dart_study/dto_mapper/dto/photo_dto.dart';
+import 'package:modu_3_dart_study/dto_mapper/model/photo.dart';
+
+extension DtoToModel on PhotoDto {
+  Photo toPhoto() {
+    int convertedId = -1; // 잘못된 값이 설정되었을 경우 -1(오류값)으로 설정
+    Type convertedType;
+
+    if (id is int) {
+      convertedId = id as int;
+    }
+    if (id is String) {
+      convertedId = int.parse(id as String);
+    }
+
+    switch (type?.toLowerCase()) {
+      case 'article':
+        convertedType = Type.Article;
+      case 'image':
+        convertedType = Type.Image;
+      case 'video':
+        convertedType = Type.Video;
+      default:
+        convertedType = Type.Unknown;
+    }
+
+    return Photo(
+      id: convertedId,
+      type: convertedType,
+      title: title == '' ? 'unknown' : (title ?? 'unknown'),
+      url: url == '' ? 'unknown' : (url ?? 'unknown'),
+      caption: caption == '' ? 'unknown' : (caption ?? 'unknown'),
+      content: content == '' ? 'unknown' : (content ?? 'unknown'),
+      createdAt: DateTime.utc(
+        int.parse(createdAt.substring(0, 4)),
+        int.parse(createdAt.substring(5, 7)),
+        int.parse(createdAt.substring(8, 10)),
+      ),
+    );
+  }
+}
