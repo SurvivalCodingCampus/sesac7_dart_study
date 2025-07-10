7ba2b1649a2a98b425a04fc658114026e73f1a45
wtoshm <wtoshm@gmail.com>
2025-07-10 19:57:50 +0900

    feat: Implement freezed and json_serializable for UserModel

--- lib/assignments/2025-07-09/model/user_model.dart
+++ lib/assignments/2025-07-09/model/user_model.dart
@@ -1,6 +1,18 @@
-class UserModel {
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+@freezed
+@JsonSerializable()
+class UserModel with _$UserModel {
+  @override
   final String email;
+  @override
   final String password;
 
   UserModel({required this.email, required this.password});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 }
