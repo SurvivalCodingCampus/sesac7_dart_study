2bb7bc24fc53a0fde22ad8e7697017f1e48eefd7
Hagmin.Lee <iilhm2001@naver.com>
2025-07-10 12:13:49 +0900

    chore: 클래스 일부 수정

--- lib/dto_mapper/mapper/photo_mapper.dart
+++ lib/dto_mapper/mapper/photo_mapper.dart
@@ -3,14 +3,15 @@ import 'package:modu_3_dart_study/dto_mapper/model/photo.dart';
 
 extension DtoToModel on PhotoDto {
   Photo toPhoto() {
-    int convertedId = -1; // 잘못된 값이 설정되었을 경우 -1(오류값)으로 설정
-    Type convertedType;
+    final int convertedId;
+    final Type convertedType;
 
     if (id is int) {
       convertedId = id as int;
-    }
-    if (id is String) {
+    } else if (id is String) {
       convertedId = int.tryParse(id as String) ?? -1;
+    } else {
+      convertedId = -1; // 잘못된 값이 설정되었을 경우 -1(오류값)으로 설정
     }
 
     switch (type?.toLowerCase()) {
@@ -31,7 +32,7 @@ extension DtoToModel on PhotoDto {
       url: url == '' ? 'unknown' : (url ?? 'unknown'),
       caption: caption == '' ? 'unknown' : (caption ?? 'unknown'),
       content: content == '' ? 'unknown' : (content ?? 'unknown'),
-      createdAt: DateTime.utc(
+      createdAt: DateTime(
         int.parse(createdAt.substring(0, 4)),
         int.parse(createdAt.substring(5, 7)),
         int.parse(createdAt.substring(8, 10)),
