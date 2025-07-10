16bda903b5df64fa51d11572dbbd03da9a86daa8
sehwan <sehwanq7@gmail.com>
2025-07-10 22:51:22 +0900

    chore: user_repository_impl.dart 테스트코드 삭제

--- lib/20250710/repository/user_repository_impl.dart
+++ lib/20250710/repository/user_repository_impl.dart
@@ -87,37 +87,3 @@ class UserRepositoryImpl implements UserRepository {
     }
   }
 }
-
-void main() async {
-  final UserDataSource userDataSource = UserDataSourceImpl(client: null);
-  final UserRepository userRepository = UserRepositoryImpl(userDataSource);
-
-  final data = await userRepository.getUser(2);
-  print('data : $data');
-
-/*  final data = await userRepository.getUsers();
-  print('data : $data');*/
-
-/*  final data = await userRepository.createUsers({
-    "id": 11,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {"lat": "-37.3159", "lng": "81.1496"},
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets",
-    },
-  });
-
-  print('data : $data');*/
-}
