a382393d30631f9e1f3cfffa91edf51cec80ed39
sehwan <sehwanq7@gmail.com>
2025-07-11 09:12:24 +0900

    refactor: User, Address 매퍼 로직 간소화
    - Nullable 필드에 대한 기본값 처리를 보다 간결하게 수정
      - `if (geo == null)`과 같은 조건문 대신 `?.` 연산자와 `??` 연산자를 활용하여 코드를 단축

--- lib/20250710/mapper/user_mapper.dart
+++ lib/20250710/mapper/user_mapper.dart
@@ -21,46 +21,28 @@ extension CompanyMapper on CompanyDto {
 
 extension AddressMapper on AddressDto {
   Address toAddress() {
-    final Geo geoInfo;
-
-    if(geo == null) {
-      geoInfo = Geo(lat: '', lng: '');
-    } else {
-      geoInfo = geo!.toGeo();
-    }
-
     return Address(street: street ?? '', suite: suite ?? '', city: city ?? '',
-        zipcode: zipcode ?? '', geo: geoInfo);
+        zipcode: zipcode ?? '', geo: geo?.toGeo() ??  Geo(lat: '', lng: ''));
   }
 }
 
 extension UserMapper on UserDto {
   User toUser() {
-    final Address addressInfo;
-    final Company companyInfo;
-
-    if(address == null) {
-      addressInfo = Address(street: '', suite: '', city: '', zipcode: '',
-          geo: Geo(lng: '', lat: ''));
-    } else {
-      addressInfo = address!.toAddress();
-    }
-
-    if(company == null) {
-      companyInfo = Company(name: '', catchPhrase: '', bs: '');
-    } else {
-      companyInfo = company!.toCompany();
-    }
-
     return User(
       id: id ?? 0,
       name: name ?? '',
       username: username ?? '',
       email: email ?? '',
-      address: addressInfo,
+      address: address?.toAddress() ?? Address(
+        street: '',
+        suite: '',
+        city: '',
+        zipcode: '',
+        geo: Geo(lat: '', lng: ''),
+      ),
       phone: phone ?? '',
       website: website ?? '',
-      company: companyInfo
+      company: company?.toCompany() ?? Company(name: '', catchPhrase: '', bs: '')
     );
   }
 }
